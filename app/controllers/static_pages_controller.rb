d9a4ec9272015c664ae59c6d0abca0d101c8aba0
Ben <robinsonbena@gmail.com>
2022-07-29 13:06:00 -0700

    set route for static page account_type.html.erb and point sign up link to that page. Each button for account type passes a value for that account type in params to the devise new registration form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,8 @@ class StaticPagesController < ApplicationController
 
   def home
   end
+
+  def account_select
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'profile_reviews/show'
+  get '/account_select', to: 'static_pages#account_select'
   
   devise_scope :user do
     get '/users/staff_sign_up' 
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   root 'static_pages#home'
+
 
   resource :adopter_profile, only: [:new, :create, :show, :edit, :update ], as: 'profile'
   resolve('adopter_profile') { [:adopter_profile] }
