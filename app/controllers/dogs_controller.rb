620c5d44bb17dfadb59dcd1f40b5a4bd16910467
Ben <robinsonbena@gmail.com>
2022-07-29 16:30:22 -0700

    create dogs controller, view folder and set resources dogs in routes

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,2 @@
+class DogsController < ApplicationController
+end
---
+++ test/controllers/dogs_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DogsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
