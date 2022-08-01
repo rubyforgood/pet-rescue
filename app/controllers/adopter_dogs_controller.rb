f04fd0919006e3683097586aff2d1c71a0d86ce5
Ben <robinsonbena@gmail.com>
2022-08-01 11:12:52 -0700

    Add #index and #show actions to adoptable_dogs controller and add Get routes for each. Tested and works as expected. The index action uses an outer left join to return dogs that do not have an associated adoption, i.e., are adoptable

--- app/controllers/adopter_dogs_controller.rb
+++
@@ -1,2 +0,0 @@
-class AdopterDogsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/account_select', to: 'static_pages#account_select'
-  get 'profile_review', to: 'profile_reviews#show'
+  get '/profile_review', to: 'profile_reviews#show'
+  get '/adoptable_dogs', to: 'adoptable_dogs#index'
+  get '/adoptable_dogs/:id', to: 'adoptable_dogs#show', as: 'adoptable_dog'
 
-  devise_scope :user do
-    get '/users/staff_sign_up'
-  end
+  # devise_scope :user do
+  #   get '/users/staff_sign_up'
+  # end
 
   devise_for :users, controllers: { registrations: 'registrations' }
 
