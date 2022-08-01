cddca78f6c28ba4b7d0d9822cd69a0de611682c2
Ben <robinsonbena@gmail.com>
2022-07-31 17:05:51 -0700

    Add before filter and private methods to check users are staff, verified, and belong to the same organization as the dog before allowing REST actions and CRUD of fog posts

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,54 +1,74 @@
 class DogsController < ApplicationController
-# before filter for edit, update, destroy to check user is in same organization
-# before filter for all to check user is staff or admin
+  before_action :verified_staff
 
-def index
-  @dogs = Dog.where(:organization_id == current_user.staff_account.organization_id)
-end
-
-def new
-  @dog = Dog.new
-end
-
-def edit
-  @dog = Dog.find(params[:id])
-end
-
-def show
-  @dog = Dog.find(params[:id])
-end
-
-def create
-  @dog = Dog.new(dog_params)
-
-  if @dog.save
-    redirect_to root_path, notice: 'Dog saved successfully.'
-  else
-    render :new, status: :unprocessable_entity
+  def index
+    @dogs = Dog.where(organization_id: current_user.staff_account.organization_id)
   end
-end
 
-def update
-  @dog = Dog.find(params[:id])
-
-  if @dog.update(dog_params)
-    redirect_to @dog
-  else
-    render :edit, status: :unprocessable_entity
+  def new
+    @dog = Dog.new
   end
-end
 
-def destroy
-  @dog = Dog.find(params[:id])
-  @dog.destroy
+  def edit
+    @dog = Dog.find(params[:id])
+    return if same_organization?(@dog)
 
-  redirect_to root_path, status: :see_other
-end
+    redirect_to dogs_path, notice: 'Staff can only interact with dogs in their organization.'
+  end
 
-private
+  def show
+    @dog = Dog.find(params[:id])
+    return if same_organization?(@dog)
 
-def dog_params
-  params.require(:dog).permit(:organization_id, :name, :age)
-end
+    redirect_to dogs_path, notice: 'Staff can only interact with dogs in their organization.'
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+
+    if @dog.save
+      redirect_to dogs_path, notice: 'Dog saved successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+
+    if same_organization?(@dog) && @dog.update(dog_params)
+      redirect_to @dog
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+
+    if same_organization?(@dog) && @dog.destroy
+      redirect_to dogs_path, status: :see_other
+    else
+      redirect_to root_path, notice: 'Error.'
+    end
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:organization_id, :name, :age)
+  end
+
+  # check before all actions that user has staff account
+  def verified_staff
+    return if current_user.staff_account.verified
+
+    redirect_to root_path, notice: 'Unauthorized action.'
+  end
+
+  # use in update and destroy to ensure staff belongs to same org as dog
+  def same_organization?(dog)
+    current_user.staff_account.organization_id == dog.organization_id
+  end
 
 end
