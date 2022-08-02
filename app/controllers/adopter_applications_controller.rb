40adbeb37e6cb75872a32ae352a625e6cdefff4e
Ben <robinsonbena@gmail.com>
2022-08-02 10:10:11 -0700

    Adoption Applications
    Add partial for applications in adopter profile views that shows a panel in the profile if adoptions exist. Remove index action from adopter_applications controller as it is not required.

--- app/controllers/adopter_applications_controller.rb
+++ app/controllers/adopter_applications_controller.rb
@@ -1,41 +1,25 @@
 class AdopterApplicationsController < ApplicationController
   before_action :authenticate_user!
   before_action :adopter_with_profile
-  
-  def index
-
-  end
-
-  # def new
-
-  # end
 
   # only create if an application does not exist
+  # this is ugly. Refactor...
   def create
-    if AdopterApplication.where(adopter_account_id: params[:adopter_account_id], 
+    if AdopterApplication.where(adopter_account_id: params[:adopter_account_id],
                                 dog_id: params[:dog_id]).exists?
-      redirect_to adoptable_dog_path(params[:dog_id]), notice: 'You have already applied to adopt this dog.'
+      redirect_to adoptable_dog_path(params[:dog_id]),
+                  notice: 'Application already exists.'
     else
       @application = AdopterApplication.new(application_params)
       if @application.save
-        redirect_to profile_path, notice: 'You have applied to adopt this pooch!'
+        redirect_to profile_path, notice: 'Application submitted.'
       else
-        render :profile_path, status: :unprocessable_entity, notice: 'Error. Try again.'
+        render adoptable_dog_path(params[:dog_id]),
+               status: :unprocessable_entity,
+               notice: 'Error. Please try again.'
       end
     end
   end
-
-  # def edit
-
-  # end
-
-  # def update
-
-  # end
-
-  # def show
-
-  # end
 
   def destroy
 
