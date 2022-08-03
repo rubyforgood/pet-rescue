78f3b1687216a6cce80d161e7fe23fb11c5ec544
Ben <robinsonbena@gmail.com>
2022-08-03 14:56:46 -0700

    Add views for adoption_application_review for index, edit and partial to render search results. Add index action query to find dogs with applications, and add form to index view to show dropdowns for dog name and application status

--- app/controllers/adoption_application_reviews_controller.rb
+++ app/controllers/adoption_application_reviews_controller.rb
@@ -1,2 +1,16 @@
 class AdoptionApplicationReviewsController < ApplicationController
+
+  def index
+    # return list of dogs from staff's organization that have applications
+    @dogs = Dog.includes(:adopter_applications).where.not(adopter_applications: {id: nil})
+
+    @adopter_applications = adopter_applications
+  end
+
+  private
+
+  def adopter_applications
+
+  end
+
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,5 @@
 class Dog < ApplicationRecord
   belongs_to :organization
-  has_many :applications
+  has_many :adopter_applications
   has_one :adoption
 end
