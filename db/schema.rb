99b297b2b97c966d23c21b67b070e30eb14a527d
Ben <robinsonbena@gmail.com>
2022-08-04 21:15:51 -0700

    Add notes column to adopter_applications table and add markup and form to #edit for adopter_applications for staff updates.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_04_181025) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_05_040425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_04_181025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.text "notes"
     t.index ["adopter_account_id"], name: "index_adopter_applications_on_adopter_account_id"
     t.index ["dog_id"], name: "index_adopter_applications_on_dog_id"
   end
