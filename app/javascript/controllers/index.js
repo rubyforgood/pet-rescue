a612b6a10bb6a672eed91ee8d438fe718c833946
Ben <robinsonbena@gmail.com>
2022-08-10 08:51:05 -0700

    testing stimulus carousel. Reject this change

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,12 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+
+import { application } from "stimulus"
+import Carousel from "stimulus-carousel"
+
+const application = application.start()
+application.register("carousel", Carousel)
+
+ 
