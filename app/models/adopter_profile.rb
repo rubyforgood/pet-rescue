3b80016553f2507ac5b2897d22a9619f1f63f1ba
Ben <robinsonbena@gmail.com>
2022-08-13 17:17:04 -0700

    Add validates_presence_of validation to adopter profile model to make sure values of false are being properly submitted to the database when no is selected in a boolean radio input. Thank you stack overflow.

--- app/models/adopter_profile.rb
+++ app/models/adopter_profile.rb
@@ -13,20 +13,26 @@ class AdopterProfile < ApplicationRecord
   validates :alone_weekend, presence: true
   validates :experience, presence: true, length: { maximum: 200 }
   validates :contingency_plan, presence: true, length: { maximum: 200 }
-  validates :shared_ownership, presence: true
+  validates_inclusion_of :shared_ownership, in: [true, false]
+  # validates :shared_ownership, presence: true
   validates :shared_owner, length: { maximum: 200 }
   validates :housing_type, presence: true
-  validates :fenced_access, presence: true
+  validates_inclusion_of :fenced_access, in: [true, false]
+  # validates :fenced_access, presence: true
   validates :fenced_alternative, length: { maximum: 200 }
   validates :location_day, presence: true, length: { maximum: 50 }
   validates :location_night, presence: true, length: { maximum: 50 }
-  validates :do_you_rent, presence: true
+  validates_inclusion_of :do_you_rent, in: [true, false]
+  # validates :do_you_rent, presence: true
   validates :adults_in_home, presence: true
   validates :kids_in_home, presence: true
-  validates :other_pets, presence: true
+  validates_inclusion_of :other_pets, in: [true, false]
+  # validates :other_pets, presence: true
   validates :describe_pets, length: { maximum: 200 }
-  validates :checked_shelter, presence: true
-  validates :surrendered_pet, presence: true
+  validates_inclusion_of :checked_shelter, in: [true, false]
+  # validates :checked_shelter, presence: true
+  validates_inclusion_of :surrendered_pet, in: [true, false]
+  # validates :surrendered_pet, presence: true
   validates :describe_surrender, length: { maximum: 200 }
   validates :annual_cost, presence: true
 end
