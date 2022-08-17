b43b8e6f3f64b7160a6f5130210c82fe84b43d80
Ben <robinsonbena@gmail.com>
2022-08-17 15:40:11 -0700

    Remove required fields in form and instead rely on the model validation errors on the server side to catch and display errors.

--- app/models/adopter_profile.rb
+++ app/models/adopter_profile.rb
@@ -1,12 +1,12 @@
 class AdopterProfile < ApplicationRecord
   belongs_to :adopter_account
 
-  validates :phone_number, presence: true
-  validates :contact_method, presence: true
-  validates :country, presence: true
-  validates :province_state, presence: true
-  validates :city_town, presence: true
-  validates :ideal_dog, presence: true, length: { maximum: 200 }
+  validates :phone_number, presence: { message: 'cannot be blank' }
+  validates :contact_method, presence: { message: 'must be selected' }
+  validates :country, presence: { message: 'must be selected' }
+  validates :province_state, presence: { message: 'must be selected' }
+  validates :city_town, presence: { message: 'cannot be blank' }
+  validates :ideal_dog, presence: { message: 'cannot be blank' }, length: { maximum: 200 }
   validates :lifestyle_fit, presence: true, length: { maximum: 200 }
   validates :activities, presence: true, length: { maximum: 200 }
   validates :alone_weekday, presence: true
