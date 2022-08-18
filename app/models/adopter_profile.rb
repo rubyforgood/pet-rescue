5670ec805ad483fc3ca250dc8f201f6e91db7b5c
Ben <robinsonbena@gmail.com>
2022-08-18 14:54:43 -0700

    add custom error message method to adopter profile model, update validations in model, updateadopter profile form partial to use said custom messages method

--- app/models/adopter_profile.rb
+++ app/models/adopter_profile.rb
@@ -6,35 +6,52 @@ class AdopterProfile < ApplicationRecord
   validates :contact_method, presence: true
   validates :country, presence: true
   validates :province_state, presence: true
-  validates :city_town, presence: true
-  validates :ideal_dog, presence: true, length: { maximum: 200 }
-  validates :lifestyle_fit, presence: true, length: { maximum: 200 }
-  validates :activities, presence: true, length: { maximum: 200 }
-  validates :alone_weekday, presence: true
-  validates :alone_weekend, presence: true
-  validates :experience, presence: true, length: { maximum: 200 }
-  validates :contingency_plan, presence: true, length: { maximum: 200 }
-  validates_inclusion_of :shared_ownership, in: [true, false], message: "can't be blank"
-  validates :shared_owner, length: { maximum: 200 }
-  validates :shared_owner, presence: true, if: :shared_owner_true?
+  validates :city_town, presence: { message: 'City/town cannot be blank' },
+                        length: { maximum: 50, message: '50 characters maximum' }
+  validates :ideal_dog, presence: { message: 'Fill this field' },
+                        length: { maximum: 200, message: '200 characters maximum' }
+  validates :lifestyle_fit, presence: { message: 'Fill this field' },
+                            length: { maximum: 200, message: '200 characters maximum' }
+  validates :activities, presence: { message: 'Fill this field' },
+                         length: { maximum: 200, message: '200 characters maximum' }
+  validates :alone_weekday, presence: { message: 'This field cannot be blank' }
+  validates :alone_weekend, presence: { message: 'This field cannot be blank' }
+  validates :experience, presence: { message: 'Fill this field' },
+                         length: { maximum: 200, message: '200 characters maximum' }
+  validates :contingency_plan, presence: { message: 'Fill this field' },
+                               length: { maximum: 200, message: '200 characters maximum' }
+  validates_inclusion_of :shared_ownership, in: [true, false],
+                                            message: 'Select one'
+  validates :shared_owner, presence: { message: 'Fill this field' },
+                           length: { maximum: 200, message: '200 characters maximum' },
+                           if: :shared_owner_true?
   validates :housing_type, presence: true
-  validates_inclusion_of :fenced_access, in: [true, false], message: "can't be blank"
-  validates :fenced_alternative, length: { maximum: 200 }
-  validates :fenced_alternative, presence: true, if: :fenced_access_false?
+  validates_inclusion_of :fenced_access, in: [true, false],
+                                         message: 'Select one'
+  validates :fenced_alternative, presence: { message: 'Fill this field' },
+                                 length: { maximum: 200, message: '200 characters maximum' },
+                                 if: :fenced_access_false?
   validates :location_day, presence: true, length: { maximum: 50 }
   validates :location_night, presence: true, length: { maximum: 50 }
-  validates_inclusion_of :do_you_rent, in: [true, false], message: "can't be blank"
-  validates_inclusion_of :dogs_allowed, in: [true, false], message: "can't be blank", if: :do_you_rent?
+  validates_inclusion_of :do_you_rent, in: [true, false], message: 'Select one'
+  validates_inclusion_of :dogs_allowed, in: [true, false],
+                                        message: 'Select one',
+                                        if: :do_you_rent?
   validates :adults_in_home, presence: true
   validates :kids_in_home, presence: true
-  validates_inclusion_of :other_pets, in: [true, false], message: "can't be blank"
-  validates :describe_pets, length: { maximum: 200 }
-  validates :describe_pets, presence: true, if: :other_pets?
-  validates_inclusion_of :checked_shelter, in: [true, false], message: "can't be blank"
-  validates_inclusion_of :surrendered_pet, in: [true, false], message: "can't be blank"
-  validates :describe_surrender, length: { maximum: 200 }
-  validates :describe_surrender, presence: true, if: :surrendered_pet?
-  validates :annual_cost, presence: true
+  validates_inclusion_of :other_pets, in: [true, false],
+                                      message: 'Select one'
+  validates :describe_pets, presence: { message: 'Fill this field' },
+                            length: { maximum: 200, message: '200 characters maximum' },
+                            if: :other_pets?
+  validates_inclusion_of :checked_shelter, in: [true, false],
+                                           message: 'Select one'
+  validates_inclusion_of :surrendered_pet, in: [true, false],
+                                           message: 'Select one'
+  validates :describe_surrender, presence: { message: 'Fill this field' },
+                                 length: { maximum: 200, message: '200 characters maximum' },
+                                 if: :surrendered_pet?
+  validates :annual_cost, presence: { message: 'Please provide an annual cost estimate' }
 
   def shared_owner_true?
     shared_ownership == true
@@ -68,6 +85,11 @@ class AdopterProfile < ApplicationRecord
       end
     formatted.gsub!(";", " x") # (415) 555-2671 x123
     formatted
+  end
+
+  # used in views to show only the custom error msg without leading attribute
+  def custom_messages(attribute)
+    self.errors.where(attribute)
   end
 
   private
