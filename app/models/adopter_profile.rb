8db3601f619d769ebbce8e77ebb9574ae5003519
Ben <robinsonbena@gmail.com>
2022-08-18 10:59:31 -0700

    Add phonelib gem to app and adopter profile model methods to normalize the phone number before saving to database, and to format the phone number for human readability afterwards

--- app/models/adopter_profile.rb
+++ app/models/adopter_profile.rb
@@ -1,7 +1,8 @@
 class AdopterProfile < ApplicationRecord
   belongs_to :adopter_account
+  before_save :normalize_phone
 
-  validates :phone_number, presence: true
+  validates :phone_number, phone: { possible: true }
   validates :contact_method, presence: true
   validates :country, presence: true
   validates :province_state, presence: true
@@ -55,4 +56,23 @@ class AdopterProfile < ApplicationRecord
     surrendered_pet == true
   end
 
+  def formatted_phone
+    parsed_phone = Phonelib.parse(phone_number)
+    return phone_number if parsed_phone.invalid?
+
+    formatted =
+      if parsed_phone.country_code == "1" # NANP
+        parsed_phone.full_national # (415) 555-2671;123
+      else
+        parsed_phone.full_international # +44 20 7183 8750
+      end
+    formatted.gsub!(";", " x") # (415) 555-2671 x123
+    formatted
+  end
+
+  private
+
+  def normalize_phone
+    self.phone_number = Phonelib.parse(phone_number).full_e164.presence
+  end
 end
---
+++ config/initializers/phonelib.rb
@@ -0,0 +1,2 @@
+Phonelib.default_country = "US"
+Phonelib.extension_separate_symbols = ["x", ";"]
