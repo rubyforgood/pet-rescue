6ddeb196b68b66848795f579ace6c24ed1af46ea
Ben <robinsonbena@gmail.com>
2022-08-20 09:45:39 -0700

    add counter to org_dog form partial and refactor stimulus counter controller to accept two different lengths

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -3,18 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['input', 'output']
 
-  // connect() {
-  //   console.log(this.inputTarget.value.length)
-  // }
 
-  count() {
-    const max = 199;
+  countMedium() {
+    this.count(250)
+  }
+
+  countLarge() {
+    this.count(500)
+  }
+
+  count(characters) {
+    const max = characters - 1;
     let output = this.outputTarget;
     let input = this.inputTarget;
     let counter = 0 + input.value.length;
 
-    output.innerText = `${counter}/200`;
-    counter >= 190 ? output.style.cssText='color: red;' : output.style.cssText='color:black;'
+    output.innerText = `${counter}/${characters}`;
+    counter >= (characters - 10) ? output.style.cssText='color: red;' : output.style.cssText='color:black;'
 
     if(input.value.length >= max) {
       input.value = input.value.substr(0, max);
