dfb68136129d35c1884caf37959668fd9e70d2bd
Ben <robinsonbena@gmail.com>
2022-08-22 10:37:53 -0700

    adopter profiles refactored for now

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,8 +1,4 @@
 class Adoption < ApplicationRecord
   belongs_to :dog
   belongs_to :adopter_account
-
-  def profile_nil?
-    AdopterProfile.where(adopter_account_id: current_user.adopter_account.id)[0].nil?
-  end
 end
