9ad3e124904c04d6504417ad7fa823dbfea950e5
Ben <robinsonbena@gmail.com>
2022-08-23 14:39:30 -0700

    Users table - make first_name and last_name null: false and add validations for them and :email to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
   has_one :staff_account
   has_one :adopter_account
 
