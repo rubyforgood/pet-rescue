56a8f92bc20975174a9dcb4412c3d9f2e64e31b4
Ben <robinsonbena@gmail.com>
2022-10-22 15:25:26 -0700

    create errors controller and views, add routes for each error, set config to point errors to routes instead of public error views

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module BajaPetRescue
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # BPR - send errors to routes to render custom error pages
+    config.exceptions_app = self.routes
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  match '/404', to: 'errors#not_found', via: :all
+  match '/422', to: 'errors#restricted_access', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
   root 'static_pages#home'
+
   get '/about_us', to: 'static_pages#about_us'
   get '/faq', to: 'static_pages#faq'
   get '/partners', to: 'static_pages#partners'
