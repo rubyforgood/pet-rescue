efe191053a3b3a18a531937d5a4de2c4d072c8ee
Ben <robinsonbena@gmail.com>
2022-10-29 10:59:43 -0700

    Add host to mailer config for devise because the reset password mail was not working.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # devise mailer (e.g. reset password)
+  config.action_mailer.default_url_options = { host: 'https://obscure-island-90789.herokuapp.com/' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
