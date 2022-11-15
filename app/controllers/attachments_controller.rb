d3102dff49c762feac4ce02dd03c44cf5b3f0399
Ben <robinsonbena@gmail.com>
2022-11-14 21:51:06 -0800

    try different redirect

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,6 +3,6 @@ class AttachmentsController < ApplicationController
   def purge
     @attachment = ActiveStorage::Attachment.find(params[:id])
     @attachment.purge
-    redirect_back fallback_location: dogs_path, notice: "Attachment removed"
+    redirect_to request.referrer, notice: "Attachment removed"
   end
 end
