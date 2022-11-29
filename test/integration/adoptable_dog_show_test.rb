bfff22ad2ccb9caf24a848c86a13754002320181
Ben <robinsonbena@gmail.com>
2022-11-28 18:30:03 -0800

    add setup to test and get the first test to pass for adoptable dogs

--- test/integration/adoptable_dog_show_test.rb
+++ test/integration/adoptable_dog_show_test.rb
@@ -2,20 +2,22 @@ require "test_helper"
 
 class AdoptableDogShowTest < ActionDispatch::IntegrationTest
 
-  # get dog id from fixture
-  # dog_id = Dog.where(name: '')[0].id
+  setup do
+    @dog_id = Dog.where(name: 'Applications')[0].id
+  end
 
-  # test "unauthenticated users can see dog show page and sign up button" do
-  #   get "/adoptable_dogs/#{dog_id}"
-  #   assert_response :success
-  #   #assert_select "a", "Sign up to be able to apply for this dog"
-  # end
+  # get dog id from fixture
+  test "unauthenticated users can see dog show page and sign up button" do
+    get "/adoptable_dogs/#{@dog_id}"
+    assert_response :success
+    assert_select "h4", "Create an account to apply for this dog"
+  end
 
   # test "adopter without a profile can see dog show and complete profile button" do
   #   sign_in users(:user_one)
   #   get "/adoptable_dogs/#{dog_id}"
   #   assert_response :success
-  #   #assert_select "a", "You need to complete your profile before you can apply"
+  #   assert_select "a", "You need to complete your profile before you can apply"
   # end
 
   # signed up but no profile see button that says complete profile
