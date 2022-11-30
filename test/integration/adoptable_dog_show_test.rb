a2197db0c9b25e08fe91a8786fd96c0777dfe41f
Ben <robinsonbena@gmail.com>
2022-11-29 18:20:31 -0800

    add more tests to adoptable dog show page

--- test/integration/adoptable_dog_show_test.rb
+++ test/integration/adoptable_dog_show_test.rb
@@ -6,20 +6,49 @@ class AdoptableDogShowTest < ActionDispatch::IntegrationTest
     @dog_id = Dog.where(name: 'Applications')[0].id
   end
 
-  # get dog id from fixture
-  test "unauthenticated users can see dog show page and sign up button" do
+  test "unauthenticated users see create account prompt and link" do
     get "/adoptable_dogs/#{@dog_id}"
     assert_response :success
     assert_select "h4", "Create an account to apply for this dog"
+    assert_select "a", "Create Account"
   end
 
-  # test "adopter without a profile can see dog show and complete profile button" do
-  #   sign_in users(:user_one)
-  #   get "/adoptable_dogs/#{dog_id}"
-  #   assert_response :success
-  #   assert_select "a", "You need to complete your profile before you can apply"
-  # end
+  test "adopter without a profile sees complete my profile prompt and link" do
+    sign_in users(:user_four)
+    get "/adoptable_dogs/#{@dog_id}"
+    assert_response :success
+    assert_select "h4", "Complete your profile to apply for this dog"
+    assert_select "a", "Complete my profile"
+  end
 
-  # signed up but no profile see button that says complete profile
-  # adopter with profile see button to adopt
+  test "adopter with a profile sees love this pooch question and apply button" do
+    sign_in users(:user_one)
+    get "/adoptable_dogs/#{@dog_id}"
+    assert_response :success
+    assert_select "h4", "In love with this pooch?"
+    assert_select 'form' do
+      assert_select 'button', 'Apply to Adopt'
+    end
+  end
+
+  test "staff do not see an adopt button only log out button" do
+    sign_in users(:user_two)
+    get "/adoptable_dogs/#{@dog_id}"
+    assert_response :success
+    assert_select 'form' do
+      assert_select 'button', 'Log Out'
+    end
+    assert_select 'form', count: 1
+  end
+
+  # https://stackoverflow.com/questions/7098499/how-to-use-assert-select-to-test-for-presence-of-a-given-link
+  # test "adopter with a profile sees button to apply to adopt" do
+  #   sign_in users(:user_four)
+  #   adopter_account_id = users(:user_four).adopter_account.id
+  #   get "/adoptable_dogs/#{@dog_id}"
+  #   assert_response :success
+  #   assert_select 'form[action=?]', create_my_application_path(application: { 
+  #     dog_id: @dog_id, 
+  #     adopter_account_id: adopter_account_id})
+  # end
 end
