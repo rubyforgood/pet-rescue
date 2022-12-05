4cf075b6f84596a7789d295e34a857a07c79201b
Ben <robinsonbena@gmail.com>
2022-12-04 17:46:11 -0800

    add test for form errors and add check for flash message to sign up tests

--- test/integration/user_account_test.rb
+++ test/integration/user_account_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserAccountTest < ActionDispatch::IntegrationTest
 
-  test "Adopter user can sign up with an associated adopter account" do
+  test "Adopter user can sign up with an associated adopter account and sees success flash" do
     post "/users",
       params: { user:
         {
@@ -18,11 +18,14 @@ class UserAccountTest < ActionDispatch::IntegrationTest
         },
         commit: 'Create Account' }
 
+    assert_response :redirect
+    follow_redirect!
+    assert_equal 'Welcome! You have signed up successfully.', flash[:notice]
     assert(User.find_by(first_name: 'Foo'))
     assert_equal AdopterAccount.last.user_id, User.last.id
   end
 
-  test "Staff user can sign up with an associated unverified staff account belonging to organization id 1" do
+  test "Staff user can sign up with an unverified staff account belonging to organization id 1 and see success flash" do
     # set org id to 1 to match db default value. Cannot pass org_id as it's not permitted param.
     organization = Organization.find_by(name: 'for_staff_sign_up')
     organization.id = 1
@@ -43,6 +46,9 @@ class UserAccountTest < ActionDispatch::IntegrationTest
         },
         commit: 'Create Account' }
 
+    assert_response :redirect
+    follow_redirect!
+    assert_equal 'Welcome! You have signed up successfully.', flash[:notice]
     assert(User.find_by(first_name: 'Ima'))
     assert(StaffAccount.find_by(organization_id: 1))
   end
@@ -52,5 +58,30 @@ class UserAccountTest < ActionDispatch::IntegrationTest
     assert(User.find_by(email: 'test@test123.com'))
     delete '/users'
     assert_nil(User.find_by(email: 'test@test123.com'))
+  end
+
+  test "error messages appear if sign up form is submitted without data" do 
+    post '/users',
+    params: { user: 
+      {
+        adopter_account_attributes: { 
+          user_id: '' 
+          }, 
+        email: '',
+        first_name: '',
+        last_name: '',
+        password: '',
+        password_confirmation: '',
+        tos_agreement: '0'
+      }, 
+      commit: 'Create Account'
+    }
+
+    assert_select 'div.alert', 'Please accept the Terms and Conditions'
+    assert_select 'div.alert', "Email can't be blank"
+    assert_select 'div.alert', "Password can't be blank"
+    assert_select 'div.alert', "First name can't be blank"
+    assert_select 'div.alert', "Last name can't be blank"
+    assert_select 'div.alert', "Tos agreement Please accept the Terms and Conditions"
   end
 end
