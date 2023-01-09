f5100ce4c968929e36f89bb520bd36b85aa25706
Ben <robinsonbena@gmail.com>
2023-01-08 18:02:08 -0800

    Add system test to check character counter displays for idea dog input field.

--- test/system/adopter_profile_forms_test.rb
+++ test/system/adopter_profile_forms_test.rb
@@ -1,10 +1,13 @@
 require "application_system_test_case"
 
 class AdopterProfileFormsTest < ApplicationSystemTestCase
-  test "visiting the adopter profile" do
+  test "Character counter displays after adding text to ideal dog input" do
     sign_in users(:user_one)
-    visit edit_profile_path
+    visit profile_path
 
+    click_on "Edit Profile"
     assert_selector "h1", text: "EDIT PROFILE"
+    fill_in "Briefly describe your ideal dog", with: "Big and fluffy."
+    assert_selector "div.small", text: "15/200"
   end
 end
