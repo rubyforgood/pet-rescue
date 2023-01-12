33ff11d1eec1e1339d4041636567c7724844a15e
Ben <robinsonbena@gmail.com>
2023-01-11 20:58:22 -0800

    add system test to check all dropdowns contain all options

--- test/system/adopter_profile_forms_test.rb
+++ test/system/adopter_profile_forms_test.rb
@@ -1,13 +1,16 @@
 require "application_system_test_case"
 
 class AdopterProfileFormsTest < ApplicationSystemTestCase
-  test "Conditional inputs appear after radio check and all text areas have character counter when text is entered" do
+
+  setup do
     sign_in users(:user_one)
     visit profile_path
 
     click_on "Edit Profile"
     assert_selector "h1", text: "EDIT PROFILE"
+  end
 
+  test "Conditional inputs appear after radio check and all text areas have character counter when text is entered" do
     fill_in "Briefly describe your ideal dog", with: "Big and fluffy."
     fill_in "Briefly describe your lifestyle", with: "Big and fluffy."
     fill_in "Briefly describe activities you will do with your dog", with: "Big and fluffy."
@@ -23,5 +26,14 @@ class AdopterProfileFormsTest < ApplicationSystemTestCase
     fill_in "Briefly describe who else will share dog ownership", with: "Big and fluffy."
 
     assert_selector "div.small", text: "15/200", count: 8
+  end
+
+  test "Check all dropdowns contain all exptected values" do 
+    assert_equal page.all('select#adopter_profile_contact_method option').map(&:value), %w(Phone Email WhatsApp)
+    assert_equal page.all('select#adopter_profile_alone_weekday option').map(&:value), %w[0 1 2 3 4 5 6 7 8 9 10]
+    assert_equal page.all('select#adopter_profile_alone_weekend option').map(&:value), %w[0 1 2 3 4 5 6 7 8 9 10]
+    assert_equal page.all('select#adopter_profile_housing_type option').map(&:value), %w(Acreage Detached Duplex Apartment Trailer)
+    assert_equal page.all('select#adopter_profile_adults_in_home option').map(&:value), %w[1 2 3 4 5 6 7 8 9 10]
+    assert_equal page.all('select#adopter_profile_kids_in_home option').map(&:value), %w[0 1 2 3 4 5 6 7 8 9 10]
   end
 end
