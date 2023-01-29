6349d7ae5aaed0f08c80cdc5d975a3dec91a0800
Ben <robinsonbena@gmail.com>
2023-01-28 19:30:00 -0800

    add arbre markup to active admin dashboard to show some basic metrics

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,34 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Adopter Accounts" do
+          para do 
+            "Total: #{AdopterAccount.all.count} accounts"
+            end
+          end
+        end
       end
-    end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    columns do
+      column do
+        panel "Unadopted Dogs" do
+          para do 
+            "Total: #{Dog.where.missing(:adoption).count} dogs."
+            end
+          end
+        end
+      end
+    
+    columns do
+      column do
+        panel "Adopted Dogs" do
+          para do 
+            "Total: #{Dog.includes(:adoption).where.not(adoption: { id: nil }).count} dogs."
+            end
+          end
+        end
+      end
+  end
 end
