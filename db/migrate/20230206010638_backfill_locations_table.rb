2fa95e3cdc919147e8087fa579c427076f7ac204
Ben <robinsonbena@gmail.com>
2023-02-07 21:40:14 -0800

    create migration to add Locations table and migration to backfill Locations table with existing location data from adopter profiles

--- db/migrate/20230206010638_backfill_locations_table.rb
+++ db/migrate/20230206010638_backfill_locations_table.rb
@@ -1,9 +1,12 @@
 class BackfillLocationsTable < ActiveRecord::Migration[7.0]
-  
+  # create class for table instead of referring directly to model
+  # this means any change in name is captured in migrations and a non issue
   class MigrateAdopterProfile < ActiveRecord::Base
     self.table_name = :adopter_profiles
   end
 
+  # create class for table instead of referring directly to model
+  # this means any change in name is captured in migrations and a non issue
   class MigrateLocation < ActiveRecord::Base
     self.table_name = :locations
   end
