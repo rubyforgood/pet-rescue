acf714cdaf9a1ba36f82d9f1d45bc2a2c5955b06
Ben <robinsonbena@gmail.com>
2023-02-07 21:42:39 -0800

    add another user with bogus address to seeds to test geocoding such data and add a service object to app/services that is used in the console to geocode all backfilled locations. This is done in a separate task from the migration to minimise impact on the application in production. Migrations lock the db and geocoding takes some time for each.

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,6 @@ adopter_user_one = User.new(email: 'b@c.com',
 adopter_user_one.save()
 
 adopter_account_one = AdopterAccount.create!(user_id: 2)
-# adopter_account_one.save
 
 adopter_user_two = User.create!(email: 'c@c.com',
                             first_name: 'Kamala',
@@ -47,18 +46,27 @@ adopter_user_two = User.create!(email: 'c@c.com',
 adopter_user_two.save()
 
 adopter_account_two = AdopterAccount.create!(user_id: 3)
-# adopter_account_two.save
+
+adopter_user_three = User.create!(email: 'd@c.com',
+  first_name: 'Bad',
+  last_name: 'Address',
+  password: '123456',
+  password_confirmation: '123456',
+  tos_agreement: 1)
+
+adopter_user_three.save()
+
+adopter_account_three = AdopterAccount.create!(user_id: 4)
 
 i = 1
 until i == 10 do 
-  dog = Dog.create!(organization_id: 1,
-                name: "dog #{i}",
-                age: (i + 1),
-                sex: 'Male',
-                size: 'Medium (22-57 lb)',
-                breed: 'Husky mix',
-                description: 'He just loves a run and a bum scratch at the end of the day')
-  # dog.save
+  Dog.create!(organization_id: 1,
+              name: "dog #{i}",
+              age: (i + 1),
+              sex: 'Male',
+              size: 'Medium (22-57 lb)',
+              breed: 'Husky mix',
+              description: 'He just loves a run and a bum scratch at the end of the day')
   i += 1
 end
 
@@ -100,12 +108,12 @@ profile_one = AdopterProfile.create!(adopter_account_id: 1,
                                  referral_source: 'my friends friend')
 # profile_one.save
 
-profile_two = AdopterProfile.create!(adopter_account_id: 2,
+profile_three = AdopterProfile.create!(adopter_account_id: 3,
                                  phone_number: '250 548 7721',
                                  contact_method: 'phone',
-                                 country: 'USA',
-                                 province_state: 'Nevada',
-                                 city_town: 'Reno',
+                                 country: 'Nonsense',
+                                 province_state: 'Nonsense',
+                                 city_town: 'Nonsense',
                                  ideal_dog: 'I love a dog with energy and a gentle spirit. 
                                             One that snuggles on the couch in the evening.',
                                  lifestyle_fit: 'I work from home during the week and am always
@@ -139,5 +147,46 @@ profile_two = AdopterProfile.create!(adopter_account_id: 2,
                                  visit_laventana: true,
                                  visit_dates: 'April 2 to May 7 2023',
                                  referral_source: 'my friends friend')
-# profile_two.save
+
+                                 profile_two = AdopterProfile.create!(adopter_account_id: 2,
+                                  phone_number: '250 548 7721',
+                                  contact_method: 'phone',
+                                  country: 'USA',
+                                  province_state: 'Nevada',
+                                  city_town: 'Reno',
+                                  ideal_dog: 'I love a dog with energy and a gentle spirit. 
+                                             One that snuggles on the couch in the evening.',
+                                  lifestyle_fit: 'I work from home during the week and am always
+                                                 at home. On weekends I do a lot of hiking.',
+                                  activities: 'We will go to the dog park, daily walks x 2, and
+                                             see friends with dogs regularly',
+                                  alone_weekday: 2,
+                                  alone_weekend: 1,
+                                  experience: 'I have owned many dogs an currently have three rescue
+                                             dogs and one foster dog',
+                                  contingency_plan: 'My neighbour is a good friend and has looked after
+                                                   my dogs multiple times and they get along very well',
+                                  shared_ownership: true,
+                                  shared_owner: 'My brother is often taking my dogs when his kids are over
+                                               for the weekend as they just love the dogs',
+                                  housing_type: 'Detached',
+                                  fenced_access: false,
+                                  fenced_alternative: 'I plan to always be outside with my dog and walk them at the local park',
+                                  location_day: 'In the house',
+                                  location_night: 'In the house',
+                                  do_you_rent: true,
+                                  dogs_allowed: true,
+                                  adults_in_home: 2,
+                                  kids_in_home: 1,
+                                  other_pets: true,
+                                  describe_pets: 'I have one cat that does not mind dogs at all',
+                                  checked_shelter: true,
+                                  surrendered_pet: true,
+                                  describe_surrender: 'I had to surrender a cat when I was 19 because of university',
+                                  annual_cost: '$2,000',
+                                  visit_laventana: true,
+                                  visit_dates: 'April 2 to May 7 2023',
+                                  referral_source: 'my friends friend')
+ 
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
