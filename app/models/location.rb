1f61ed5e6fa381ef780658f78c314c9a88c3f005
Ben <robinsonbena@gmail.com>
2023-02-07 21:38:42 -0800

    Add geocoder gem and set up Location model with address method and hooks to geocode a new location record

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,11 @@
 class Location < ApplicationRecord
   belongs_to :adopter_profile
+  validates :city_town, presence: true
+  geocoded_by :address
+  after_validation :geocode
+
+  # create address string for Geocoder gem to get Lat/Long
+  def address
+    [self.city_town, self.province_state, self.country].compact.join(', ')
+  end
 end
