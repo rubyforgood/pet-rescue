9dfa82d762a8b0e8d94995fcb80474376452f21b
Ben R <robinsonbena@gmail.com>
2023-03-23 20:33:00 -0700

    add lat lon to fixture; test to check /successes route works; test to check the route contains expected html elements for google maps api

--- test/integration/successes_page_test.rb
+++ test/integration/successes_page_test.rb
@@ -5,8 +5,17 @@ class SuccessesPageTest < ActionDispatch::IntegrationTest
   # setup do
   # end
 
-  test "successes page can be accessed" do
-    get "/successes"
-    assert_response :success
+  test "A list element for each adopted dog with lat lon data attributes is created" do
+    get '/successes'
+    assert_select 'ul.coordinates' do
+      assert_select 'li', { count: Adoption.all.count }
+      assert_select 'li[data-lat]', { value: "51.0866897" }
+      assert_select 'li[data-lon]', { value: "-115.3481135" }
+    end
+
   end
+
+  # make an adoption and test one more list
+  # changes lat/lon and test changed lat lon
+
 end
