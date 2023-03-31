72285931a6b669e7bceb736b50f9338cbd477831
Ben R <robinsonbena@gmail.com>
2023-03-31 16:03:46 -0700

    create new collection method for successes page index action

--- app/controllers/successes_controller.rb
+++ app/controllers/successes_controller.rb
@@ -1,7 +1,29 @@
 class SuccessesController < ApplicationController
 
   def index
-    @adoption_all = Adoption.all
+    @collection = create_collection_of_adoption_data
+    check_if_any_locations_are_identical(@collection)
+    debugger
   end
+
+  private
+
+  def create_collection_of_adoption_data
+    collection = []
+    Adoption.all.each { |record| 
+      adoption_data = {
+        latitude: record.adopter_account.adopter_profile.location.latitude,
+        longitude: record.adopter_account.adopter_profile.location.longitude,
+        dog_name: record.dog.name
+      }
+      collection << adoption_data
+    }
+    collection
+  end
+
+  def check_if_any_locations_are_identical(collection)
+    
+  end
+
 
 end
