bd90793199faae48c4a09c8ef72df3dd7c0f7fb6
Ben R <robinsonbena@gmail.com>
2023-04-14 16:19:52 -0700

    add methods to paypal response mapper and create new donation in controller #create

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,6 +1,7 @@
 class DonationsController < ApplicationController
-
-    def create
-
-    end
+	def create
+		@mapped_response = PaypalResponseMapper.new(params) if params[:source] == 'Paypal'
+		
+		@donation = Donation.create!(@mapped_response.map)
+	end
 end
