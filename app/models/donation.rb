6c303bbc0b53bdb8700871f54b7eca647b94a73d
Ben R <robinsonbena@gmail.com>
2023-04-23 15:28:21 -0700

    add donation summary to active admin dashboard

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,18 @@
 class Donation < ApplicationRecord
 	validates :amount, presence: true
 	validates :currency, presence: true
+
+  def self.sum_donations_by_currency
+    Donation.all.reduce({}) do |sums, transaction|
+      amount = transaction[:amount].to_f
+      currency = transaction[:currency]
+
+      if sums.key?(currency)
+        sums[currency] += amount
+      else
+        sums[currency] = amount
+      end
+      sums
+    end
+  end
 end
