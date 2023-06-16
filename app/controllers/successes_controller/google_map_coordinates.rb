c82bdbb210a7c00849bc869b6b9e78c65ca4c6c6
egemen-dev <egemenwasd@gmail.com>
2023-06-16 20:35:40 +0300

    Add deviations to all adoptions coordinates and create map data

--- app/controllers/successes_controller/google_map_coordinates.rb
+++ app/controllers/successes_controller/google_map_coordinates.rb
@@ -1,55 +1,35 @@
 class SuccessesController
-	# class to retrieve and prepare coordinates for Google Map API, used by successes controller
-	class GoogleMapCoordinates
-		attr_reader :map_collection, :raw_collection
+  # class to retrieve and prepare coordinates for Google Map API, used by successes controller
+  class GoogleMapCoordinates
+    attr_accessor :location, :map_data
 
-		def initialize(raw_collection=[])
-			@raw_collection = raw_collection
-			@duplicate_locations = []
-			@map_collection = []
-		end
+    def initialize(map_data = [])
+      @map_data = map_data
+    end
 
-		def create_raw_collection
-			Adoption.all.each do |adoption|
+    def genarate_map_data
+      generate_map_data_from_adoptions if map_data.empty?
+    end
 
-				lat = adoption.adopter_account.adopter_profile.location.latitude
-				lon = adoption.adopter_account.adopter_profile.location.longitude
-	
-				unless lat.nil? || lon.nil?
-					@raw_collection << {latitude: lat, longitude: lon}
-				end
-			end
-			@raw_collection
-		end
+    def generate_map_data_from_adoptions
+      Adoption.all.each do |adoption|
+        @location = adoption.adopter_account.adopter_profile.location
 
-		def find_duplicate_locations
-			@duplicate_locations = @raw_collection.group_by {|e| e}.select {|k,v| v.size > 1}.map(&:first)
-		end
-	
-		def modify_duplicate_locations
-			modified_collection = @raw_collection.clone
-	
-			@duplicate_locations.each do |location|
-				count = @raw_collection.count(location)
-				locations_to_modify = Array.new(count - 1, location)
-	
-				modified_locations = locations_to_modify.map do |loc|
-					{
-						latitude: loc[:latitude] + rand(-0.3..0.3),
-						longitude: loc[:longitude] + rand(-0.3..0.3)
-					}
-				end
-				modified_collection += modified_locations
-			end
-			modified_collection.uniq
-		end
+        lat, lon = deviate_coordinates if valid_location?
+        @map_data << { latitude: lat, longitude: lon, dog_name: adoption.dog.name, breed: adoption.dog.breed }
+      end
+      @map_data
+    end
 
-		def generate_coordinates
-			create_raw_collection if @raw_collection.empty?
-			find_duplicate_locations
-			@duplicate_locations.empty? ? @map_collection = @raw_collection :
-																		@map_collection = modify_duplicate_locations
-			@map_collection
-		end
-	end
+    def valid_location?
+      location.latitude && location.longitude
+    end
+
+    def deviate_coordinates
+      location.latitude += rand(-0.1..0.1)
+      location.longitude += rand(-0.1..0.1)
+
+      [location.latitude, location.longitude]
+    end
+  end
 end
