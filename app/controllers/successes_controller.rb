a6403f931a10fba7ae8bc3cbb150c231e5bc866a
egemen-dev <egemenwasd@gmail.com>
2023-06-16 20:33:34 +0300

    Change naming

--- app/controllers/successes_controller.rb
+++ app/controllers/successes_controller.rb
@@ -1,6 +1,6 @@
 class SuccessesController < ApplicationController
-  def index 
-    coordinates_retriever = GoogleMapCoordinates.new
-    @map_collection = coordinates_retriever.generate_coordinates
+  def index
+    google_maps_coordinates = GoogleMapCoordinates.new
+    @map_collection = google_maps_coordinates.genarate_map_data
   end
 end
