f05c1f0be7f7481bf2c814f0838560297ce50b68
egemen-dev <egemenwasd@gmail.com>
2023-06-18 19:14:55 +0300

    Rename instance variable

--- app/controllers/successes_controller.rb
+++ app/controllers/successes_controller.rb
@@ -1,6 +1,6 @@
 class SuccessesController < ApplicationController
   def index
     adoptions = Adoption.includes(:dog, adopter_account: { adopter_profile: :location })
-    @map_collection = GoogleMap.call(adoptions:).metadata
+    @google_map_metadata = GoogleMap.call(adoptions:).metadata
   end
 end
