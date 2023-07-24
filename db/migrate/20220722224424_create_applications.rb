3670c32c8829d8d8f43863466a20fa7b86f0b327
egemen-dev <egemenwasd@gmail.com>
2023-07-24 11:55:23 +0300

    Rename in db/migrate

--- db/migrate/20220722224424_create_applications.rb
+++ db/migrate/20220722224424_create_applications.rb
@@ -1,7 +1,7 @@
 class CreateApplications < ActiveRecord::Migration[7.0]
   def change
     create_table :applications do |t|
-      t.references :dog, null: false, foreign_key: true
+      t.references :pet, null: false, foreign_key: true
       t.references :adopter_account, null: false, foreign_key: true
       t.string :status
       t.timestamps
--- db/migrate/20220804181025_create_adoptions.rb
+++ db/migrate/20220804181025_create_adoptions.rb
@@ -1,7 +1,7 @@
 class CreateAdoptions < ActiveRecord::Migration[7.0]
   def change
     create_table :adoptions do |t|
-      t.references :dog, null: false, foreign_key: true
+      t.references :pet, null: false, foreign_key: true
       t.references :adopter_application, null: false, foreign_key: true
 
       t.timestamps
