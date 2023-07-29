3385ea4138fd06a2a5adc0885fe61216ddb1c2e5
Edwin Mak <edwinthinks@gmail.com>
2023-07-29 09:56:46 -0400

    Fix standardrb errors

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
-require 'capybara'
-require 'capybara/cuprite'
+require "capybara"
+require "capybara/cuprite"
 
-require 'evil_systems'
+require "evil_systems"
 
 EvilSystems.initial_setup
 # To pass in driver_options to cuprite you can do the following:
