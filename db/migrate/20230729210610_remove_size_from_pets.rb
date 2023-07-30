eb0c66197d0c5088c0697d87ce826e2342298fb2
Ben R <robinsonbena@gmail.com>
2023-07-30 10:07:23 -0700

    migrations
    remove size from pets table and create new weights table with reference to pet

---
+++ db/migrate/20230729210610_remove_size_from_pets.rb
@@ -0,0 +1,7 @@
+class RemoveSizeFromPets < ActiveRecord::Migration[7.0]
+  class RemoveNameFromPets < ActiveRecord::Migration[7.0]
+    def change
+      remove_column :pets, :size, :string
+    end
+  end
+end
