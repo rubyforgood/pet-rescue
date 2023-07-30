28635322e4f2ba385fdb1c4fcd7293a57e38ac5b
albertchae <albertchae@users.noreply.github.com>
2023-07-30 06:43:30 -0700

    Denormalize organization_id onto Match (#113)
    I wanted to add `acts_as_tenant(:organization, through: :pet)` to the
    Match model, but it looks like `through` is only supported for HABTM
    associations and not belongs_to/has_one. This seems to be intentional
    design in acts_as_tenant https://github.com/ErwinM/acts_as_tenant/issues/293#issuecomment-1238567771
    
    So I added `organization_id` to `Match`, but a risk of doing that is
    when there's a discrepancy between the denormalized tenant_id and the
    tenant_id reachable through the object graph. In this case that would be
    when `match.organization_id` does not match `match.pet.organization_id`.
    
    My preferred way of dealing with that is adding a composite foreign key
    including the tenant_id so that you can use the database to establish and
    guarantee "chain of trust" up to the tenant. Here that would look like
    ```
    ALTER TABLE matches
    ADD CONSTRAINT fk_pet_id_organization_id_to_pets
    FOREIGN KEY (pet_id, organization_id)
    REFERENCES pets (id, organization_id);
    ```
    
    (N.B. we also need `add_index :pets, [:id, :organization_id], unique: true` because
    composite foreign keys require uniqueness on the set of columns they're
    pointing to).
    
    We have to use raw DDL because rails currently doesn't support composite foreign keys
    in their migration helpers. However, an issue with this approach is that
    the `schema.rb` dump doesn't support foreign keys. We'd get something
    like `add_foreign_key "matches", "pets", name: "fk_pet_id_organization_id_to_pets"` which
    has no mention of the columns that make up the composite key. We could
    get around this by changing this project to use a `structure.sql` schema
    dump
    https://blog.appsignal.com/2020/01/15/the-pros-and-cons-of-using-structure-sql-in-your-ruby-on-rails-application.html,
    but I don't think we want to make this change now. So I instead just
    added an ActiveRecord validation to guarantee the organization_id
    matches.
    
    P.S. it looks like native rails support for composite keys is coming https://github.com/rails/rails/issues/47593

--- app/models/match.rb
+++ app/models/match.rb
@@ -6,11 +6,13 @@
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  adopter_account_id :bigint           not null
+#  organization_id    :bigint           not null
 #  pet_id             :bigint           not null
 #
 # Indexes
 #
 #  index_matches_on_adopter_account_id  (adopter_account_id)
+#  index_matches_on_organization_id     (organization_id)
 #  index_matches_on_pet_id              (pet_id)
 #
 # Foreign Keys
@@ -21,10 +23,21 @@
 class Match < ApplicationRecord
   belongs_to :pet
   belongs_to :adopter_account
+  belongs_to :organization
+
+  validate :belongs_to_same_organization_as_pet
 
   after_create_commit :send_checklist_reminder
 
   def send_checklist_reminder
     MatchMailer.checklist_reminder(self).deliver_later
+  end
+
+  private
+
+  def belongs_to_same_organization_as_pet
+    if organization_id != pet.organization_id
+      errors.add(:organization_id, "must belong to same organization as pet")
+    end
   end
 end
