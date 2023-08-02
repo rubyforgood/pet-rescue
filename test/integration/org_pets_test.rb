73c37291144246790ee7301f8a7de954db36e19a
Jade Stewart <jadekstewart3@gmail.com>
2023-08-02 12:22:44 -0600

    Modify tests to use factories

--- test/integration/org_pets_test.rb
+++ test/integration/org_pets_test.rb
@@ -1,18 +1,11 @@
 require "test_helper"
 
 class OrgPetsTest < ActionDispatch::IntegrationTest
-  setup do
-    @pet = pets(:one)
-    @org_id = users(:verified_staff_one).staff_account.organization_id
-  end
-
-  teardown do
-    :after_teardown
-  end
-
   test "adopter user cannot access org pets index" do
-    sign_in users(:adopter_with_profile)
+    sign_in create(:user, :adopter_with_profile)
+
     get "/pets/new"
+
     assert_response :redirect
     follow_redirect!
     check_messages
@@ -21,8 +14,10 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
   end
 
   test "unverified staff cannot access org pets index" do
-    sign_in users(:unverified_staff)
+    sign_in create(:user, :unverified_staff)
+
     get "/pets/new"
+
     assert_response :redirect
     follow_redirect!
     check_messages
@@ -31,12 +26,13 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
   end
 
   test "unverified staff cannot post to org pets" do
-    sign_in users(:unverified_staff)
+    sign_in create(:user, :unverified_staff)
 
-    post "/pets",
+    post(
+      "/pets",
       params: {pet:
         {
-          organization_id: organizations(:one).id.to_s,
+          organization_id: create(:organization).id.to_s,
           name: "TestPet",
           age: "3",
           sex: "Female",
@@ -45,6 +41,7 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
           description: "A lovely little pooch this one.",
           append_images: [""]
         }}
+      )
 
     assert_response :redirect
     follow_redirect!
@@ -53,24 +50,29 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
   end
 
   test "verified staff can access org pets index" do
-    sign_in users(:verified_staff_one)
+    sign_in create(:user, :verified_staff)
+
     get "/pets/new"
+
     check_messages
   end
 
   test "verified staff can access pet/new" do
-    sign_in users(:verified_staff_one)
+    sign_in create(:user, :verified_staff)
+
     get "/pets/new"
+
     check_messages
   end
 
   test "verified staff can create a new pet post" do
-    sign_in users(:verified_staff_one)
+    sign_in create(:user, :verified_staff)
 
-    post "/pets",
+    post(
+      "/pets",
       params: {pet:
         {
-          organization_id: organizations(:one).id.to_s,
+          organization_id: create(:organization).id.to_s,
           name: "TestPet",
           birth_date: DateTime.current - (4.months + 15.days),
           sex: "Female",
@@ -79,6 +81,7 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
           description: "A lovely little pooch this one.",
           append_images: [""]
         }}
+        )
 
     assert_response :redirect
     follow_redirect!
@@ -88,12 +91,15 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
   end
 
   test "verified staff can edit a pet post" do
-    sign_in users(:verified_staff_one)
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    sign_in create(:user, :verified_staff, staff_account: staff_account)
 
-    patch "/pets/#{@pet.id}",
+    patch(
+      "/pets/#{create(:pet, organization: organization).id}",
       params: {pet:
         {
-          organization_id: organizations(:one).id.to_s,
+          organization_id: organization.id.to_s,
           name: "TestPet",
           age: "7",
           sex: "Female",
@@ -102,6 +108,7 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
           description: "A lovely little pooch this one.",
           append_images: [""]
         }}
+        )
 
     assert_response :redirect
     follow_redirect!
@@ -111,31 +118,41 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
   end
 
   test "verified staff can pause pet" do
-    sign_in users(:verified_staff_one)
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    pet = create(:pet, organization: organization)
+    sign_in create(:user, :verified_staff, staff_account: staff_account)
 
-    patch "/pets/#{@pet.id}",
+    patch(
+      "/pets/#{pet.id}",
       params: {pet: {
         application_paused: true
       }}
+    )
 
     assert_response :redirect
     follow_redirect!
     check_messages
     assert_equal "Pet updated successfully.", flash[:notice]
-    @pet.reload
+    pet.reload
 
-    assert @pet.application_paused
+    assert pet.application_paused
   end
 
   test "in dropdown, pause reason is selected for paused pet" do
-    sign_in users(:verified_staff_one)
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    pet = create(:pet, organization: organization)
+    sign_in create(:user, :verified_staff, staff_account: staff_account)
 
-    patch "/pets/#{@pet.id}",
+    patch(
+      "/pets/#{pet.id}",
       params: {pet:
         {
           application_paused: true,
           pause_reason: "paused_until_further_notice"
         }}
+        )
 
     assert_response :redirect
     follow_redirect!
@@ -148,32 +165,42 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
   end
 
   test "verified staff can unpause a paused pet" do
-    @pet = pets(:paused_application)
-    sign_in users(:verified_staff_one)
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    pet = create(:pet, organization: organization, application_paused: true)
+    sign_in create(:user, :verified_staff, staff_account: staff_account)
 
-    assert @pet.application_paused
+    assert pet.application_paused
 
-    patch "/pets/#{@pet.id}",
+    patch(
+      "/pets/#{pet.id}",
       params: {pet:
         {
           application_paused: "false"
         }}
+      )
 
     assert_response :redirect
     follow_redirect!
     check_messages
     assert_equal "Pet updated successfully.", flash[:notice]
-    @pet.reload
+    pet.reload
 
-    assert_not @pet.application_paused
-    assert_equal @pet.pause_reason, "not_paused"
+    assert_not pet.application_paused
+    assert_equal pet.pause_reason, "not_paused"
   end
 
   test "verified staff can upload multiple images" do
-    sign_in users(:verified_staff_one)
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    pet = create(:pet, organization: organization)
 
-    assert_difference "@pet.images_attachments.length", 2 do
-      patch "/pets/#{@pet.id}",
+    sign_in create(:user, :verified_staff, staff_account: staff_account)
+    
+
+    assert_difference "pet.images_attachments.length", 2 do
+      patch(
+        "/pets/#{pet.id}",
         params: {pet:
                   {
                     append_images:
@@ -182,51 +209,99 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
                       fixture_file_upload("test2.png", "image/png")
                     ]
                   }}
+      )
 
-      @pet.reload
+      pet.reload
     end
   end
 
   test "verified staff can delete an image" do
-    sign_in users(:verified_staff_one)
-    pet_image = @pet.images_attachments.first
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    pet = create(:pet, organization: organization)
 
-    assert_difference "@pet.images_attachments.length", -1 do
-      delete "/attachments/#{pet_image.id}/purge",
+    sign_in create(:user, :verified_staff, staff_account: staff_account)
+
+    patch(
+      "/pets/#{pet.id}",
+      params: {pet:
+                {
+                  append_images:
+                  [
+                    fixture_file_upload("test.png", "image/png"),
+                    fixture_file_upload("test2.png", "image/png")
+                  ]
+                }}
+    )
+
+    pet.reload
+
+    pet_image = pet.images_attachments.first
+   
+    assert_difference "pet.images_attachments.length", -1 do
+      delete(
+        "/attachments/#{pet_image.id}/purge",
         params: {id: pet_image.id.to_s},
-        headers: {"HTTP_REFERER" => "http://www.example.com/pets/#{@pet.id}"}
+        headers: {"HTTP_REFERER" => "http://www.example.com/pets/#{pet.id}"}
+      )
 
       assert_response :redirect
       follow_redirect!
       check_messages
       assert_equal "Attachment removed", flash[:notice]
 
-      @pet.reload
+      pet.reload
     end
   end
 
   test "user that is not verified staff cannot delete an image attachment" do
-    sign_in users(:adopter_with_profile)
-    pet_image = @pet.images_attachments.first
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    pet = create(:pet, organization: organization)
 
-    assert_no_difference "@pet.images_attachments.length" do
-      delete "/attachments/#{pet_image.id}/purge",
+    sign_in create(:user, :verified_staff, staff_account: staff_account)
+
+    patch(
+      "/pets/#{pet.id}",
+      params: {pet:
+                {
+                  append_images:
+                  [
+                    fixture_file_upload("test.png", "image/png"),
+                    fixture_file_upload("test2.png", "image/png")
+                  ]
+                }}
+    )
+
+    pet.reload
+
+    sign_in create(:user, :adopter_with_profile)
+    pet_image = pet.images_attachments.first
+
+    assert_no_difference "pet.images_attachments.length" do
+      delete(
+        "/attachments/#{pet_image.id}/purge",
         params: {id: pet_image.id.to_s},
-        headers: {"HTTP_REFERER" => "http://www.example.com/pets/#{@pet.id}"}
-
+        headers: {"HTTP_REFERER" => "http://www.example.com/pets/#{pet.id}"}
+      )
+      
       follow_redirect!
       check_messages
       assert_equal "/", path
       assert_equal "Unauthorized action.", flash[:alert]
 
-      @pet.reload
+      pet.reload
     end
   end
 
   test "verified staff can delete pet post" do
-    sign_in users(:verified_staff_one)
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    pet = create(:pet, organization: organization)
 
-    delete "/pets/#{@pet.id}"
+    sign_in create(:user, :verified_staff)
+
+    delete "/pets/#{pet.id}"
 
     assert_response :redirect
     follow_redirect!
@@ -236,7 +311,9 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
 
   # test org pets index page filter for adoption status
   test "verified staff accessing org pets index without selection param see all unadopted pets" do
-    sign_in users(:verified_staff_one)
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    sign_in create(:user, :verified_staff)
 
     get "/pets"
     check_messages
@@ -244,28 +321,50 @@ class OrgPetsTest < ActionDispatch::IntegrationTest
   end
 
   test "verified staff accessing org pets index with selection param seeking adoption see all unadopted pets" do
-    sign_in users(:verified_staff_one)
-    get "/pets",
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+
+    sign_in create(:user, :verified_staff)
+
+    get(
+      "/pets",
       params: {selection: "Seeking Adoption"}
+    )
+
     check_messages
     assert_select "div.col-lg-4", {count: Pet.unadopted_pets(@org_id).count}
   end
 
   test "verified staff accessing org pets index with selection param adopted see all adopted pets" do
-    sign_in users(:verified_staff_one)
-    get "/pets",
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+
+    sign_in create(:user, :verified_staff)
+
+    get("/pets",
       params: {selection: "Adopted"}
+    )
+
     check_messages
     assert_select "div.col-lg-4", {count: Pet.adopted_pets(@org_id).count}
   end
 
   # test org pets index page filter for pet name
   test "verified staff accessing org pets index with a pet id see that pet only" do
-    sign_in users(:verified_staff_one)
-    get "/pets",
-      params: {pet_id: @pet.id}
+    organization = create(:organization)
+    staff_account = create(:staff_account, organization: organization)
+    pet = create(:pet, organization: organization)
+
+
+    sign_in create(:user, :verified_staff)
+
+    get("/pets",
+      params: {pet_id: pet.id}
+    )
+
     check_messages
     assert_select "div.col-lg-4", {count: 1}
-    assert_select "h5", "Applications"
+
+    assert_select "h5", pet.name
   end
 end
