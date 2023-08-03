e7f8ea6d43e18207da85d9b7f2e3d675fced9f5b
Gary Tou <gary@garytou.com>
2023-08-03 01:33:29 -0700

    Add tests

--- test/mailers/match_mailer_test.rb
+++ test/mailers/match_mailer_test.rb
@@ -2,7 +2,10 @@ require "test_helper"
 
 class MatchMailerTest < ActionMailer::TestCase
   test "checklist reminder" do
-    email = MatchMailer.checklist_reminder(matches(:adoption_one))
+    match = matches(:adoption_one)
+    match.assign_checklist_template(checklist_templates(:one))
+
+    email = MatchMailer.checklist_reminder(match)
 
     assert_emails 1 do
       email.deliver_now
@@ -12,5 +15,15 @@ class MatchMailerTest < ActionMailer::TestCase
     assert_match(/Checklist Reminder/, email.subject)
     assert_match(/#{matches(:adoption_one).pet.name}/, email.subject)
     assert_match(/#{matches(:adoption_one).pet.name}/, email.body.encoded)
+  end
+
+  test "checklist reminder with no assignments" do
+    match = matches(:adoption_one)
+
+    email = MatchMailer.checklist_reminder(match)
+
+    assert_emails 0 do
+      email.deliver_now
+    end
   end
 end
