bb03fa2eda5bdd322b2ed0d22c5759c359718e7b
Sree P <nsiwnf@users.noreply.github.com>
2023-08-03 19:21:05 -0700

    code-review

--- app/services/multi_tenant_service.rb
+++ app/services/multi_tenant_service.rb
@@ -1,17 +1,17 @@
 class MultiTenantService
-  def initialize(service_org)
-    @service_org = service_org.to_sym
+  def initialize(service_org_name)
+    @service_org_name = service_org_name.to_sym
   end
 
   def default_email
-    Rails.configuration.email_from[:default][service_org]
+    Rails.configuration.email_from[:default][service_org_name]
   end
 
   def contact_email
-    Rails.configuration.email_from[:contact][service_org]
+    Rails.configuration.email_from[:contact][service_org_name]
   end
 
   private
 
-  attr_accessor :service_org
+  attr_accessor :service_org_name
 end
