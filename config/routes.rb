6b8b7f2c10d202dffe344cf467e0ca075a91d87c
Egemen Öztürk <93445248+egemen-dev@users.noreply.github.com>
2023-08-05 00:51:57 +0300

    Implement dynamic country state dropdown (#136)
    * Add country_state json file
    
    * Add set_countries_and_states method to controller
    
    * Add Stimulus controller and targets
    
    * Pass down the variable to partial
    
    * Create Stimulus controller and related functions to update dropdown
    
    * Remove method from controller
    
    * Remove instance var from view
    
    * Finalize stimulus controller
    
    * Move json file to javascript/helpers and export
    
    * Add initial value to data attribue if any
    
    * Update commets
    
    * Add Mexico to js file
    
    * Implement dropdown with hotwire turbo streams
    
    * Add requestjs-rails gem
    
    * Utilize requestjs-rails in Stimulus controller
    
    * Create contries controller and route
    
    * Move turbo_streame.erb to new directory
    
    * Update path value on data attribute
    
    * Write test for countries controller
    
    * Change route
    
    This change caused some changes in controller naming as well
    
    * Try fixing conflicts
    
    * Run standardrb --fix

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   resources :profile_reviews, only: [:show]
   resources :successes, only: [:index]
 
+  resources :countries, only: [] do
+    resources :states, only: [:index]
+  end
+
   match "/404", to: "errors#not_found", via: :all
   match "/422", to: "errors#restricted_access", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
---
+++ test/controllers/states_controller_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class StatesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:adopter_with_profile)
+  end
+
+  test "should return turbo stream with the states in it" do
+    name = "adopter[address_attributes][state]"
+    target = "adopter_profile_location_attributes_province_state"
+
+    get country_states_path(:country),
+      headers: {"Accept" => "text/vnd.turbo-stream.html"},
+      params: {country: "CA", target:, name:}
+
+    assert_response :success
+    assert_select "turbo-stream[action='replace'][target='#{target}']" do
+      assert_select "template" do
+        assert_select "select[name='#{name}']" do
+          assert_select "option", 13
+        end
+      end
+    end
+  end
+end
