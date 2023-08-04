b387e792c1c7be739255b3f89c77dc965f16cbbc
Eric Tillberg <Thrillberg@users.noreply.github.com>
2023-08-03 22:55:18 -0400

    baby steps

--- test/system/adopter_application_edit_test.rb
+++ test/system/adopter_application_edit_test.rb
@@ -2,13 +2,17 @@ require "application_system_test_case"
 
 class AdopterApplicationEditTest < ApplicationSystemTestCase
   test "Clicking the information icon makes the information box appear and disappear" do
-    application = create(:adopter_application, :adoption_pending)
-    user = create(:user, :verified_staff)
+    organization = create(:organization)
+    pet = create(:pet, organization: organization)
+    adopter = create(:adopter_account, :with_adopter_profile)
+    application = create(:adopter_application, :adoption_pending, pet: pet, adopter_account: adopter)
+    user = create(:user, :verified_staff, staff_account: create(:staff_account, organization: organization))
+    adopter_name = adopter.user.first_name + " " + adopter.user.last_name
     sign_in user
 
     visit edit_adopter_application_path(application.id)
 
-    assert_selector "h1", text: "Ben Jo's application for Adopted"
+    assert_selector "h1", text: "#{adopter_name}'s application for #{pet.name}"
     assert_selector "p.explanation", count: 0
 
     find("img.ms-2").click do |image|
