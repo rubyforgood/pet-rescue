8d5556ae4e4a3e7111ed94176aeba544a9d40f2d
Gary Tou <gary@garytou.com>
2023-08-25 19:03:52 -0700

    Add test

--- test/models/checklist_template_item_test.rb
+++ test/models/checklist_template_item_test.rb
@@ -8,5 +8,6 @@ class ChecklistTemplateItemTest < ActiveSupport::TestCase
   context "validations" do
     should validate_presence_of(:name)
     should validate_presence_of(:expected_duration_days)
+    should validate_numericality_of(:expected_duration_days).is_greater_than_or_equal_to(0)
   end
 end
