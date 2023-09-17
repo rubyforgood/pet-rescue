770118089a75e7fb8ca3cea08eded5e91ee22529
Ben R <robinsonbena@gmail.com>
2023-09-17 14:35:44 -0700

    add shoulda tests for associations and validations and remove unit tests testing role adding and removing

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,28 +1,14 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class RoleTest < ActiveSupport::TestCase
-  def setup
-    @organization = create(:organization)
-    @staff_account = create(:staff_account, organization: @organization)
+  context 'associations' do
+    should have_and_belong_to_many(:staff_accounts).join_table(:staff_accounts_roles)
+    should belong_to(:resource).optional
   end
 
-  test "assigns the admin role to staff_account within organization scope" do
-    @staff_account.add_role(:admin, @organization)
-
-    assert @staff_account.has_role?(:admin, @organization)
-  end
-
-  test "does not assign the admin role to staff_account outside organization scope" do
-    other_organization = Organization.create(name: "Other Organization")
-    @staff_account.add_role(:admin, other_organization)
-
-    refute @staff_account.has_role?(:admin, @organization)
-  end
-
-  test "removes the admin role from staff_account" do
-    @staff_account.add_role(:admin, @organization)
-    @staff_account.remove_role(:admin, @organization)
-
-    refute @staff_account.has_role?(:admin, @organization)
+  context 'validations' do
+    should validate_inclusion_of(:resource_type).in_array(Rolify.resource_types).allow_nil
   end
 end
