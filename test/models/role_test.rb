7286f925d5c7076cb1123eb363a25f81e958998a
Ben R <robinsonbena@gmail.com>
2023-09-17 14:57:33 -0700

    lint fix

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -3,12 +3,12 @@
 require "test_helper"
 
 class RoleTest < ActiveSupport::TestCase
-  context 'associations' do
+  context "associations" do
     should have_and_belong_to_many(:staff_accounts).join_table(:staff_accounts_roles)
     should belong_to(:resource).optional
   end
 
-  context 'validations' do
+  context "validations" do
     should validate_inclusion_of(:resource_type).in_array(Rolify.resource_types).allow_nil
   end
 end
