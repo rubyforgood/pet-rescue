b9cb76732c2d5bff54644b74437dc209bfe76b33
Edwin Mak <edwinthinks@gmail.com>
2023-09-20 08:06:43 -0500

    Add link to access other rescues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  #
+  # Returns the path to the home page of the organization.
+  #
+  #
+  def organization_home_path(organization)
+    home_index_path(script_name: "/#{organization.slug}")
+  end
+
 end
