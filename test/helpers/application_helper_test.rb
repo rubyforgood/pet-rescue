dd2c00d65ef0db4a274399fadaa7467fcc01cc23
Edwin Mak <edwinthinks@gmail.com>
2023-09-20 08:17:40 -0500

    Add basic test

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  context "#organization_home_path" do
+    should "return the correct path with the organization slug" do
+      organization = create(:organization)
+
+      assert_equal home_index_path(script_name: "/#{organization.slug}"), organization_home_path(organization)
+      assert_match /#{organization.slug}/, organization_home_path(organization)
+    end
+  end
+end
