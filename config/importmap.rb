8e96219ea61bb391b4900316a9f971dbee378030
Edwin Mak <edwinthinks@gmail.com>
2023-09-26 06:49:04 -0500

    Add simplebar which was missing to get sidebar styling correct

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,7 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
+pin "simplebar", to: "https://ga.jspm.io/npm:simplebar@6.2.5/dist/index.mjs"
+pin "can-use-dom", to: "https://ga.jspm.io/npm:can-use-dom@0.1.0/index.js"
+pin "lodash-es", to: "https://ga.jspm.io/npm:lodash-es@4.17.21/lodash.js"
+pin "simplebar-core", to: "https://ga.jspm.io/npm:simplebar-core@1.2.4/dist/index.mjs"
