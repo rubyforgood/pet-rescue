a4ed92df9206841d98ce623cce1e6a30f6d7ec20
Erin Claudio <eclaudio@gmail.com>
2023-09-29 11:57:21 -0400

    Update with recent changes to main

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,9 +16,9 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # protected
 
