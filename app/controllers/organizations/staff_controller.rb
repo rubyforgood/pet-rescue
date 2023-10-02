41976213f4231139331ce61e326cda8f60363521
Sree P <nsiwnf@users.noreply.github.com>
2023-10-01 19:30:41 -0500

    Adding staff index page

--- app/controllers/organizations/staff_controller.rb
+++ app/controllers/organizations/staff_controller.rb
@@ -1,6 +1,9 @@
 class Organizations::StaffController < Organizations::BaseController
   layout "dashboard"
 
+  before_action :organization_admin
+
   def index
+    @staff_accounts = StaffAccount.where(organization: current_tenant)
   end
 end
