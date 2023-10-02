cea1ca49d26bca6495c6a0cfcd52eb350e071ce6
Sree P <34173394+nsiwnf@users.noreply.github.com>
2023-10-02 12:42:59 -0500

    Adding staff index page (#223)
    Co-authored-by: Edwin Mak <edwinthinks@gmail.com>

--- app/controllers/organizations/staff_controller.rb
+++ app/controllers/organizations/staff_controller.rb
@@ -1,6 +1,9 @@
 class Organizations::StaffController < Organizations::BaseController
   layout "dashboard"
 
+  before_action :require_organization_admin
+
   def index
+    @staff_accounts = StaffAccount.all
   end
 end
