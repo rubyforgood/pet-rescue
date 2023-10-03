85e7a937a1089eabeff1b03e638d1c956dc52c70
Yuri Carvalho Pains <yuricarvalhop@gmail.com>
2023-10-03 11:57:13 +0200

    Remove location fields to Organization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_150909) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_094720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -160,9 +160,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_150909) do
 
   create_table "organizations", force: :cascade do |t|
     t.string "name"
-    t.string "city"
-    t.string "country"
-    t.string "zipcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
