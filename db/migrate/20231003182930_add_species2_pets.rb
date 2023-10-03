f13e12ca7d4cf2ae9b5c81d79fd666698133a9dd
Jade Stewart <jadekstewart3@gmail.com>
2023-10-03 14:11:20 -0600

    Add species to pets table and set up enum in the pet model

---
+++ db/migrate/20231003182930_add_species2_pets.rb
@@ -0,0 +1,5 @@
+class AddSpecies2Pets < ActiveRecord::Migration[7.0]
+  def change
+    add_column :pets, :species, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_150909) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_182930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -182,6 +182,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_150909) do
     t.integer "weight_from", null: false
     t.integer "weight_to", null: false
     t.string "weight_unit", null: false
+    t.integer "species"
     t.index ["organization_id"], name: "index_pets_on_organization_id"
   end
 
