13766ebf6ff44dc9e9ef58c982003e238f48b8ee
Ben R <robinsonbena@gmail.com>
2023-10-04 16:37:35 -0700

    lint fix

--- app/mailers/organization_mailer.rb
+++ app/mailers/organization_mailer.rb
@@ -2,7 +2,7 @@ class OrganizationMailer < ApplicationMailer
   def create_new_org_and_admin(tenant_org)
     @organization = params[:organization]
     @user = params[:user]
-    mail(from: 'default@petrescue.org',
+    mail(from: "default@petrescue.org",
       to: @user.email,
       subject: "Pet Rescue - New Organization Sign Up")
   end
