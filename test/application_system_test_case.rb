ea9e8b5e133f2141dcae98f44a2fa9c565d09f77
Piotr Borowiec <p.borowiec01@gmail.com>
2023-10-04 17:27:24 +0200

    Increase process timeout in tests (#237)
    * Fix flaky test by increasing process_timeout
    
    * Fix deprecation warning by bumping city-state gem

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,9 +5,7 @@ require "capybara/cuprite"
 
 require "evil_systems"
 
-EvilSystems.initial_setup
-# To pass in driver_options to cuprite you can do the following:
-# EvilSystems.initial_setup(driver_options: { process_timeout: 20 })
+EvilSystems.initial_setup(driver_options: {process_timeout: 20})
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :evil_cuprite
