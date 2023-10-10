51706ee6bc8160de19960c7c9f7931807a027042
marlena-b <marlena.borowiec01@gmail.com>
2023-10-10 16:32:05 +0200

    Allow admins to invite other staff

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string           not null
+#  invitation_accepted_at :datetime
+#  invitation_created_at  :datetime
+#  invitation_limit       :integer
+#  invitation_sent_at     :datetime
+#  invitation_token       :string
+#  invitations_count      :integer          default(0)
+#  invited_by_type        :string
 #  last_name              :string           not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
@@ -13,11 +20,15 @@
 #  tos_agreement          :boolean
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  invited_by_id          :bigint
 #  organization_id        :bigint
 #
 # Indexes
 #
 #  index_users_on_email                 (email) UNIQUE
+#  index_users_on_invitation_token      (invitation_token) UNIQUE
+#  index_users_on_invited_by            (invited_by_type,invited_by_id)
+#  index_users_on_invited_by_id         (invited_by_id)
 #  index_users_on_organization_id       (organization_id)
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
@@ -35,7 +46,7 @@ class User < ApplicationRecord
     end
   end
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: "registrations",
-    sessions: "users/sessions"
-  }
+    sessions: "users/sessions",
+    invitations: 'organizations/invitations'
+}
 
   resources :adoptable_pets, only: [:index, :show]
   resource :adopter_profile, except: :destroy, as: "profile"
