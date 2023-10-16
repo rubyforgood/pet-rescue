c4ccc0158709fed9870b4275d6f1fcd060802f04
yuricarvalhop <yuricarvalhop@gmail.com>
2023-10-16 20:54:21 +0200

    Move avatarable tests to shared module

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,35 +1,10 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "shared/avatarable_shared_tests"
 
 class UserTest < ActiveSupport::TestCase
-  context "avatarable" do
-    should "behave as avatarable" do
-      assert_includes User.included_modules, Avatarable
-
-      assert subject, respond_to?(:append_avatar=)
-    end
-
-    context "validations" do
-      should "append error if avatar is too big" do
-        fixture_file.stubs(:size).returns(2.megabytes)
-
-        subject.avatar.attach(io: fixture_file, filename: "test.png")
-
-        refute subject.valid?
-        assert_includes subject.errors[:avatar], "size must be between 10kb and 1Mb"
-      end
-
-      should "append error if avatar is too small" do
-        fixture_file.stubs(:size).returns(1.kilobyte)
-
-        subject.avatar.attach(io: fixture_file, filename: "test.png")
-
-        refute subject.valid?
-        assert_includes subject.errors[:avatar], "size must be between 10kb and 1Mb"
-      end
-    end
-  end
+  include AvatarableSharedTests
 
   context "associations" do
     should have_one(:staff_account).dependent(:destroy)
