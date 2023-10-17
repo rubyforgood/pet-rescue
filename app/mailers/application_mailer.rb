9898607930a1e9641cc674c0d6e25914083c1aad
Piotr Borowiec <p.borowiec01@gmail.com>
2023-10-17 11:42:49 +0200

    Prefix links in mailers with organization slug

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  before_action :set_tenant_path
+
+  private
+
+  def set_tenant_path
+    return unless Current.tenant
+
+    default_url_options[:script_name] = "/#{Current.tenant.slug}"
+  end
 end
