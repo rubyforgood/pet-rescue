a25f9cabd387ec334d6ded291f3f270be2e2e3ec
Piotr Borowiec <p.borowiec01@gmail.com>
2023-10-17 11:42:49 +0200

    Prefix links in mailers with organization slug

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  around_action :with_organization_path, if: -> { Current.organization }
+
+  private
+
+  def with_organization_path
+    default_url_options[:script_name] = "/#{Current.organization.slug}"
+    yield
+    default_url_options[:script_name] = nil
+  end
 end
