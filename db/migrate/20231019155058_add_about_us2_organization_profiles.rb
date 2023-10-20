fdac06f9928464bbbfbb3c79a7353f484f6ccefb
Jade Stewart <jadekstewart3@gmail.com>
2023-10-20 14:18:45 -0600

    remove null false from migration file and migrate

--- db/migrate/20231019155058_add_about_us2_organization_profiles.rb
+++ db/migrate/20231019155058_add_about_us2_organization_profiles.rb
@@ -1,5 +1,5 @@
 class AddAboutUs2OrganizationProfiles < ActiveRecord::Migration[7.0]
   def change
-    add_column :organization_profiles, :about_us, :string, null: false
+    add_column :organization_profiles, :about_us, :string
   end
 end
