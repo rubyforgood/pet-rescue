1c9fc85aac90c9b0fb3e144b397c16e0582561c8
Jade Stewart <jadekstewart3@gmail.com>
2023-10-20 14:37:53 -0600

    standard rb

--- app/models/organization_profile.rb
+++ app/models/organization_profile.rb
@@ -3,7 +3,7 @@
 # Table name: organization_profiles
 #
 #  id              :bigint           not null, primary key
-#  about_us        :string           
+#  about_us        :string
 #  email           :string
 #  phone_number    :string
 #  created_at      :datetime         not null
