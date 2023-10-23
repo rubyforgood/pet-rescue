fcc4e799be240160f75ba88d8d19dd47c91aa650
Jade Stewart <114014697+jadekstewart3@users.noreply.github.com>
2023-10-23 13:43:17 -0700

    Add column 2 org profiles (#289)
    * Create migration file to add about us to organization profiles table, and add column to seed files
    
    * add about us field for organization_profiles
    
    * add validation for about us
    
    * add validation for about us
    
    * standard
    
    * Remove validation that we didnt need
    
    * remove null false from migration file and migrate
    
    * standard rb
    
    * add validation for about us
    
    * Add back in accidentially removed validation
    
    * Migrate updated migration file
    
    * Add null false back into correct place and migrate
    
    * remove not null comment
    
    * standard rb
    
    * merge main

--- test/factories.rb
+++ test/factories.rb
@@ -103,7 +103,7 @@ FactoryBot.define do
   factory :organization_profile do
     email { Faker::Internet.email }
     phone_number { Faker::PhoneNumber.phone_number }
-
+    about_us { Faker::Lorem.paragraph(sentence_count: 4) }
     location
 
     trait :with_organization do
