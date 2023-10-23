b322d972ce4f054d0456cbc5c43faa74b401133d
Jade Stewart <114014697+jadekstewart3@users.noreply.github.com>
2023-10-23 13:43:17 -0700

    Add column 2 org profiles (#289)
    * Create migration file to add about us to organization profiles table, and add column to seed files
    
    * add about us field for organization_profiles
    
    * add validation for about us
    
    * add validation for about us
    
    * standard
    
    * Remove validation that we didnt need
    
    * remove null false from migration file and migrate
    
    * standard rb
    
    * add validation for about us
    
    * Add back in accidentially removed validation
    
    * Migrate updated migration file
    
    * Add null false back into correct place and migrate
    
    * remove not null comment
    
    * standard rb
    
    * merge main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_115817) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_19_155058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -166,6 +166,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_115817) do
     t.bigint "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "about_us"
     t.index ["location_id"], name: "index_organization_profiles_on_location_id"
     t.index ["organization_id"], name: "index_organization_profiles_on_organization_id"
   end
