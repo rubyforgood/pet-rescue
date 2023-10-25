b34c0c349840bcc6b078a3c7b3de24ca50d39260
marlena-b <marlena.borowiec01@gmail.com>
2023-10-25 13:42:04 +0200

    Add uniq index to organization slug

---
+++ db/migrate/20231025113121_add_uniq_index_to_organizations_slug.rb
@@ -0,0 +1,6 @@
+class AddUniqIndexToOrganizationsSlug < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+  def change
+    add_index :organizations, :slug, unique: true, algorithm: :concurrently
+  end
+end
