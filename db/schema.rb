b95e18a8678148668fe4f23c5ac56333bc4ded96
Marlena Borowiec <96994176+marlena-b@users.noreply.github.com>
2023-10-26 10:24:49 +0200

    Add uniq index to organization slug (#302)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_113924) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_113121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -176,6 +176,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_113924) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.index ["slug"], name: "index_organizations_on_slug", unique: true
   end
 
   create_table "pets", force: :cascade do |t|
