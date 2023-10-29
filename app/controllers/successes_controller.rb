c0728b18aa862703923971a871ca0ca2d3467dce
Ben R <robinsonbena@gmail.com>
2023-10-29 12:01:24 -0700

    remove related logic

--- app/controllers/successes_controller.rb
+++
@@ -1,6 +0,0 @@
-class SuccessesController < ApplicationController
-  def index
-    adoptions = Match.eager_load(:pet, adopter_account: {adopter_profile: :location})
-    @google_map_metadata = GoogleMap::DataBuilder.new(adoptions).data
-  end
-end
