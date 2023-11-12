5aff313a98685581695092cd546ade7bb3ec22b5
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-11-12 08:26:05 -0500

    add helper method to determine application status class, update applications partial to use helper

--- app/helpers/adoption_application_reviews_helper.rb
+++ app/helpers/adoption_application_reviews_helper.rb
@@ -1,2 +1,12 @@
 module AdoptionApplicationReviewsHelper
+  def application_status_classes(application)
+    status_classes = { awaiting_review: "bg-dark-primary", 
+                        under_review: "bg-primary", 
+                        adoption_pending: "text-black bg-light-warning", 
+                        withdrawn: "text-black bg-gray-200", 
+                        successful_applicant: "text-black bg-light-success", 
+                        adoption_made: "text-black bg-light-success" }
+
+    status_classes[application.status.to_sym]
+  end
 end
