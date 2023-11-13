583333752f7b3b6607963207a917c5cc17607b92
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-11-13 16:43:25 -0500

    Update org_pets_with_apps to include adopter_account, user

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -92,7 +92,7 @@ class Pet < ApplicationRecord
 
   # all pets under an organization with applications and no adoptions
   def self.org_pets_with_apps(staff_org_id)
-    Pet.org_pets(staff_org_id).includes(:adopter_applications).where
+    Pet.org_pets(staff_org_id).includes(adopter_applications: [adopter_account: [:user]]).where
       .not(adopter_applications: {id: nil}).includes(:match)
       .where(match: {id: nil})
   end
