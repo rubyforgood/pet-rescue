846a1029e20b35ec36c3d4ec18cf6fb12d8b225e
Jade Stewart <jadekstewart3@gmail.com>
2023-11-13 10:17:21 -0800

    Add null: false to migration file

--- db/migrate/20231110194816_add_status2_pets.rb
+++ db/migrate/20231110194816_add_status2_pets.rb
@@ -1,5 +1,5 @@
 class AddStatus2Pets < ActiveRecord::Migration[7.0]
   def change
-    add_column :pets, :placement_type, :integer
+    add_column :pets, :placement_type, :integer, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -192,7 +192,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_194816) do
     t.integer "weight_to", null: false
     t.string "weight_unit", null: false
     t.integer "species", null: false
-    t.integer "placement_type"
+    t.integer "placement_type", null: false
     t.index ["organization_id"], name: "index_pets_on_organization_id"
   end
 
