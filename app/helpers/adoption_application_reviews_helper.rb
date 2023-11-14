cf9ce29777e12ee1d41da2b3319d22b4f4ce6d70
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-11-14 05:51:41 -0500

    run standard

--- app/helpers/adoption_application_reviews_helper.rb
+++ app/helpers/adoption_application_reviews_helper.rb
@@ -1,11 +1,11 @@
 module AdoptionApplicationReviewsHelper
   def application_status_classes(application)
-    status_classes = { awaiting_review: "bg-dark-primary", 
-                        under_review: "bg-dark-info", 
-                        adoption_pending: "text-black bg-light-warning", 
-                        withdrawn: "text-black bg-gray-200", 
-                        successful_applicant: "text-black bg-light-success", 
-                        adoption_made: "text-black bg-light-success" }
+    status_classes = {awaiting_review: "bg-dark-primary",
+                      under_review: "bg-dark-info",
+                      adoption_pending: "text-black bg-light-warning",
+                      withdrawn: "text-black bg-gray-200",
+                      successful_applicant: "text-black bg-light-success",
+                      adoption_made: "text-black bg-light-success"}
 
     status_classes[application.status.to_sym]
   end
