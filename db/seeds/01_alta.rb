f8e654ea0afdb2cd899b005772779bee15bef956
Ben R <robinsonbena@gmail.com>
2023-11-19 13:48:25 -0800

    Make sure the adopter accounts are scoped to the correct organization otherwise we get adopter applications from users scoped to another organization which breaks the app

--- db/seeds/01_alta.rb
+++ db/seeds/01_alta.rb
@@ -5,13 +5,13 @@ orga_location = Location.create!(
   zipcode: "12345"
 )
 
-organization = Organization.create!(
+@organization = Organization.create!(
   name: "Alta Pet Rescue",
   slug: "alta",
   profile: OrganizationProfile.new(email: "alta@email.com", phone_number: "123 456 7890", location: orga_location, about_us: "We get pets into loving homes!")
 )
 
-ActsAsTenant.with_tenant(organization) do
+ActsAsTenant.with_tenant(@organization) do
   @user_staff_one = User.create!(
     email: "staff@alta.com",
     first_name: "Andy",
@@ -243,7 +243,7 @@ ActsAsTenant.with_tenant(organization) do
       notes: Faker::Lorem.paragraph,
       profile_show: true,
       status: rand(0..5),
-      adopter_account: AdopterAccount.all.sample,
+      adopter_account: AdopterAccount.joins(:user).where(users: { organization_id: @organization.id }).sample,
       pet: Pet.all.sample
     )
   end
