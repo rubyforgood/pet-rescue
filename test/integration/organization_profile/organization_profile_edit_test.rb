2deafccecc1657770059e665a8e60fbb1998346b
Jade Stewart <jadekstewart3@gmail.com>
2023-11-22 12:57:18 -0700

    Begin testing for organization profile edit

---
+++ test/integration/organization_profile/organization_profile_edit_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class OrganizationProfile::EditProfileTest < ActionDispatch::IntegrationTest
+  setup do 
+    @org = create(:organization)
+    @org_profile = create(:organization_profile, organization_id: @org.id)
+
+    admin = create(:user, :staff_admin)
+    set_organization(admin.organization)
+    sign_in admin
+    get edit_organization_profile_path(@org)
+  end
+
+  test "all expected fields are present on the edit organization profile page" do 
+    assert_select "h4", text: "Your avatar"
+    assert_select "p", text: "PNG or JPG, size must be between 10kb and 1Mb"
+
+    assert_select "label", text: "Organization name"
+    assert_select "input[name='organization_profile[organization_name]'][type='text']"
+
+    assert_select "label", text: "Phone number"
+    assert_select "input[name='organization_profile[phone_number]'][type='tel']"
+
+    assert_select "label", text: "Email"
+    assert_select "input[name='organization_profile[email]'][type='text']"
+
+    assert_select "label", text: "Country"
+    assert_select "select[name='organization_profile[location_attributes][province_state]']"
+
+    assert_select "label", text: "City/Town"
+    assert_select "input[name='organization_profile[location_attributes][city_town]'][type='text']"
+
+    assert_select "button[type='submit']", text: "Save profile"
+  end
+end
