fd529f423dc8eba0fdfcd9be20df7f6f4b14eaea
Jade Stewart <jadekstewart3@gmail.com>
2023-11-22 10:51:16 -0700

    Add delegate to org_profile model to gain access to name attribute

--- app/models/organization_profile.rb
+++ app/models/organization_profile.rb
@@ -31,6 +31,8 @@ class OrganizationProfile < ApplicationRecord
   before_save :normalize_phone
 
   validates :phone_number, phone: {possible: true, allow_blank: true}
+  
+  delegate :name, to: :organization
 
   private
 
