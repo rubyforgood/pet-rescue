8518c0d2f1eca67b322efede8b4b6f0dc5d8ea42
Jade Stewart <jadekstewart3@gmail.com>
2023-11-28 16:02:13 -0700

    Create happy path testing for organization_profile edit page

--- test/integration/organization_profile/organization_profile_edit_test.rb
+++ test/integration/organization_profile/organization_profile_edit_test.rb
@@ -3,13 +3,12 @@ require "test_helper"
 
 class OrganizationProfile::EditProfileTest < ActionDispatch::IntegrationTest
   setup do
-    @org = create(:organization)
-    @org_profile = create(:organization_profile, organization_id: @org.id)
-
-    admin = create(:user, :staff_admin)
+    @org = ActsAsTenant.test_tenant
+    @org_profile = @org.profile
+    admin = create(:user, :staff_admin, organization: @org)
     set_organization(admin.organization)
     sign_in admin
-    get edit_organization_profile_path(@org)
+    get edit_organization_profile_path(@org_profile)
   end
 
   test "all expected fields are present on the edit organization profile page" do 
@@ -28,30 +27,33 @@ class OrganizationProfile::EditProfileTest < ActionDispatch::IntegrationTest
     assert_select "label", text: "City/Town"
     assert_select "input[name='organization_profile[location_attributes][city_town]'][type='text']"
 
-    # assert_select "button[type='submit']", text: "Save profile"
+    assert_select 'input[type="submit"][value="Save profile"]'
   end
 
   test 'organization profile updates with the submission of the form' do
     patch organization_profile_path(@org_profile), params: {
       organization_profile: {
-        phone_number: "+13038947563",
         email: "happy_paws_rescue@gmail.com",
-        location_attributes: [
+        phone_number: "3038947563",
+        about_us: "Finding pets loving homes across the front range!",
+        location_attributes: {
           country: "United States",
           province_state: "Colorado",
           city_town: "Golden"
-        ] 
+        }
       }
     }
-
     @org_profile.reload
 
     assert_response :redirect
     follow_redirect!
     assert_response :success
 
-    assert_equal "+13038947563", @org_profile.phone_number
     assert_equal "happy_paws_rescue@gmail.com", @org_profile.email
+    assert_equal "+13038947563", @org_profile.phone_number
+    assert_equal "Finding pets loving homes across the front range!", @org_profile.about_us
     assert_equal "United States", @org_profile.location.country
+    assert_equal "Colorado", @org_profile.location.province_state
+    assert_equal "Golden", @org_profile.location.city_town
   end
 end
