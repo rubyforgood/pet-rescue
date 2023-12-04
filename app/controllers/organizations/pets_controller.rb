78df9d912c792479738ab69a29220198391b5f42
Alvaro Sanchez <hello@alvarosanchez.dev>
2023-12-04 12:37:53 -0600

    Added is_published toggle button to pet form (Affects new and edit)

--- app/controllers/organizations/pets_controller.rb
+++ app/controllers/organizations/pets_controller.rb
@@ -1,101 +1,107 @@
-class Organizations::PetsController < Organizations::BaseController
-  before_action :set_pet, only: [:show, :edit, :update, :destroy]
-  before_action :verified_staff
-  before_action :set_nav_tabs, only: [:show]
+# frozen_string_literal: true
 
-  after_action :set_reason_paused_to_none, only: [:update]
-  layout "dashboard"
+module Organizations
+  class PetsController < Organizations::BaseController
+    before_action :set_pet, only: %i[show edit update destroy]
+    before_action :verified_staff
+    before_action :set_nav_tabs, only: [:show]
 
-  def index
-    @q = Pet.ransack(params[:q])
-    @pets = @q.result
-  end
+    after_action :set_reason_paused_to_none, only: [:update]
+    layout 'dashboard'
 
-  def new
-    @pet = Pet.new
-  end
-
-  def edit
-    return if pet_in_same_organization?(@pet.organization_id)
-
-    redirect_to pets_path, alert: "This pet is not in your organization."
-  end
-
-  def show
-    @active_tab = determine_active_tab
-    @pause_reason = @pet.pause_reason
-    return if pet_in_same_organization?(@pet.organization_id)
-
-    redirect_to pets_path, alert: "This pet is not in your organization."
-  end
-
-  def create
-    @pet = Pet.new(pet_params)
-
-    if @pet.save
-      redirect_to pets_path, notice: "Pet saved successfully."
-    else
-      render :new, status: :unprocessable_entity
+    def index
+      @q = Pet.ransack(params[:q])
+      @pets = @q.result
     end
-  end
 
-  def update
-    if pet_in_same_organization?(@pet.organization_id) && @pet.update(pet_params)
-      redirect_to @pet, notice: "Pet updated successfully."
-    else
-      render :edit, status: :unprocessable_entity
+    def new
+      @pet = Pet.new
     end
-  end
 
-  def destroy
-    @pet = Pet.find(params[:id])
+    def edit
+      return if pet_in_same_organization?(@pet.organization_id)
 
-    if pet_in_same_organization?(@pet.organization_id) && @pet.destroy
-      redirect_to pets_path, notice: "Pet deleted.", status: :see_other
-    else
-      redirect_to pets_path, alert: "Error."
+      redirect_to pets_path, alert: 'This pet is not in your organization.'
     end
-  end
 
-  private
+    def show
+      @active_tab = determine_active_tab
+      @pause_reason = @pet.pause_reason
+      return if pet_in_same_organization?(@pet.organization_id)
 
-  def pet_params
-    params.require(:pet).permit(:organization_id,
-      :name,
-      :birth_date,
-      :sex,
-      :species,
-      :breed,
-      :description,
-      :application_paused,
-      :pause_reason,
-      :weight_from,
-      :weight_to,
-      :weight_unit,
-      :species,
-      :placement_type,
-      append_images: [])
-  end
+      redirect_to pets_path, alert: 'This pet is not in your organization.'
+    end
 
-  def set_pet
-    @pet = Pet.find(params[:id])
-  end
+    def create
+      @pet = Pet.new(pet_params)
 
-  # update Pet pause_reason to not paused if applications resumed
-  def set_reason_paused_to_none
-    return unless @pet.application_paused == false
+      if @pet.save
+        redirect_to pets_path, notice: 'Pet saved successfully.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
 
-    @pet.pause_reason = 0
-    @pet.save!
-  end
+    def update
+      if pet_in_same_organization?(@pet.organization_id) && @pet.update(pet_params)
+        redirect_to @pet, notice: 'Pet updated successfully.'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
 
-  def set_nav_tabs
-    @nav_tabs = [
-      {name: "Summary", path: pet_path(@pet)}
-    ]
-  end
+    def destroy
+      @pet = Pet.find(params[:id])
 
-  def determine_active_tab
-    ["tasks", "applications", "files"].include?(params[:active_tab]) ? params[:active_tab] : "overview"
+      if pet_in_same_organization?(@pet.organization_id) && @pet.destroy
+        redirect_to pets_path, notice: 'Pet deleted.', status: :see_other
+      else
+        redirect_to pets_path, alert: 'Error.'
+      end
+    end
+
+    private
+
+    def pet_params
+      params.require(:pet).permit(
+        :application_paused,
+        :birth_date,
+        :breed,
+        :description,
+        :is_published,
+        :name,
+        :organization_id,
+        :pause_reason,
+        :placement_type,
+        :sex,
+        :species,
+        :weight_from,
+        :weight_to,
+        :weight_unit,
+        append_images: []
+      )
+    end
+
+    def set_pet
+      @pet = Pet.find(params[:id])
+    end
+
+    # update Pet pause_reason to not paused if applications resumed
+    def set_reason_paused_to_none
+      return unless @pet.application_paused == false
+
+      @pet.pause_reason = 0
+      @pet.save!
+    end
+
+    def set_nav_tabs
+      @nav_tabs = [
+        { name: 'Summary', path: pet_path(@pet) }
+      ]
+    end
+
+    def determine_active_tab
+      %w[tasks applications files].include?(params[:active_tab]) ? params[:active_tab] : 'overview'
+    end
   end
 end
