1261e17c5cda65796e1139ee7f3313cab2758a0c
Erin Claudio <eclaudio@gmail.com>
2023-12-07 20:52:48 -0500

    Added new tasks table (#361)
    * Added new tasks table
    
    * Added migration
    
    * Added controller for tasks
    
    * Update Final newline added
    
    * Added partial and tasks infor in pet file
    
    * Added partial and task for factorybot
    
    * Added tasks helper
    
    * Added model test
    
    * Updated tests
    
    * Added the edit file
    
    * Added turno stream files and edit
    
    * Added task.rb and testing file
    
    * Update removed comments from failing tests
    
    * Update added in new edit button
    
    * Update edit button on files partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     home_index_path(script_name: "/#{organization.slug}")
   end
 
+  def path_to_partial(active_tab)
+    "pets/#{active_tab}"
+  end
+
   def current_organization_name
     if Current.organization.present?
       Current.organization.name
