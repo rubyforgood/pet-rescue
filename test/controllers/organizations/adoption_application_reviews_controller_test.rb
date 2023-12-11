5807304185e82b9796af72cef3d31505986eced0
phonghpham <phizzy@gmail.com>
2023-12-11 17:39:53 -0600

    add test for filtering by pet name

--- test/controllers/organizations/adoption_application_reviews_controller_test.rb
+++ test/controllers/organizations/adoption_application_reviews_controller_test.rb
@@ -1,18 +1,33 @@
 require "test_helper"
 
 class Organizations::AdoptionApplicationReviewsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = create(:user, :verified_staff)
-    set_organization(@user.organization)
-    sign_in @user
-  end
+  context "Filtering adoption applications" do
+    setup do
+      @user = create(:user, :verified_staff)
+      set_organization(@user.organization)
+      sign_in @user
+    end
 
-  teardown do
-    :after_teardown
-  end
+    teardown do
+      :after_teardown
+    end
 
-  test "should respond successfully" do
-    get adoption_application_reviews_url
-    assert_response :success
+    context "by pet name" do
+      setup do
+        @pet1 = create(:pet, name: "Pango", organization: @user.staff_account.organization)
+        @pet2 = create(:pet, name: "Tycho", organization: @user.staff_account.organization)
+        adopter_account1 = create(:adopter_account, :with_adopter_profile, organization: @user.staff_account.organization)
+        adopter_account2 = create(:adopter_account, :with_adopter_profile, organization: @user.staff_account.organization)
+        create(:adopter_application, pet: @pet1, adopter_account: adopter_account1)
+        create(:adopter_application, pet: @pet2, adopter_account: adopter_account2)
+      end
+
+      should "return applications for a specific pet name" do
+        get adoption_application_reviews_url, params: {q: { pet_name_cont: "Pango" } }
+        assert_response :success
+        assert_match "Pango", @response.body
+        refute_match "Tycho", @response.body
+      end
+    end
   end
 end
