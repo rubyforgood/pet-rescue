7977ec5fc6de6aa137dd9233510893a18715be3b
Sree P <nsiwnf@users.noreply.github.com>
2023-12-29 16:59:26 -0800

    By default dartsass only adds application.scss

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -30,6 +30,12 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  Rails.application.config.dartsass.builds = {
+    "application.scss"  => "application.css",
+    "feather.css"  => "feather.css"
+  }
+
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
