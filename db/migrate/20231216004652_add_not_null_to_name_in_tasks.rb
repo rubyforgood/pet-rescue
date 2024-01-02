441a2be50c649ca48accd207afd478ed9e59abd3
Alisa <82609260+MooseCowBear@users.noreply.github.com>
2024-01-02 18:39:11 -0500

    391 remove strong migrations gem (#400)
    * remove strong migrations gem and initializer
    
    * remove safety_assured blocks from migrations

--- db/migrate/20231216004652_add_not_null_to_name_in_tasks.rb
+++ db/migrate/20231216004652_add_not_null_to_name_in_tasks.rb
@@ -1,7 +1,5 @@
 class AddNotNullToNameInTasks < ActiveRecord::Migration[7.0]
   def change
-    safety_assured do
-      change_column_null :tasks, :name, false
-    end
+    change_column_null :tasks, :name, false
   end
 end
