859bab66f2a3467ab8007608efa6dd5854d6113c
Sree P <nsiwnf@users.noreply.github.com>
2024-01-02 08:48:34 -0800

    Revert dart config - we do only want application css. Changed config for defaul asset compile

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -27,13 +27,8 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  Rails.application.config.dartsass.builds = {
-    "application.scss" => "application.css",
-    "feather.css" => "feather.css"
-  }
+  # Fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
