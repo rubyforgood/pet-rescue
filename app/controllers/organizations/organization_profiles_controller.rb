764b9b2a8967ab0bdd4c2b2a26f1832481aa13cf
Jade Stewart <jadekstewart3@gmail.com>
2024-01-04 15:47:17 -0700

    Update how organization profile is called in controller, add commented out destroy action, may put into production, awaiting confirmation

--- app/controllers/organizations/organization_profiles_controller.rb
+++ app/controllers/organizations/organization_profiles_controller.rb
@@ -2,17 +2,23 @@ class Organizations::OrganizationProfilesController < Organizations::BaseControl
   layout "dashboard"
 
   def edit
-    @organization_profile = Current.organization.profile
+    @organization_profile = OrganizationProfile.first
   end
 
   def update
-    @organization_profile = Current.organization.profile
+    @organization_profile = OrganizationProfile.first
     if @organization_profile.update(organization_profile_params)
       redirect_to dashboard_index_path, notice: "Your profile has been updated!"
     else
       render :edit, status: :unprocessable_entity
     end
   end
+
+  # def destroy
+  #   organization_profile = OrganizationProfile.first
+  #   organization_profile.destroy
+  #   redirect_to dashboard_index_path
+  # end
 
   private
 
