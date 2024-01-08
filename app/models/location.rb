3098b2b33ce007b8b5bd0dd793c6592d0ee315e0
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2024-01-08 17:37:26 +0200

    * added more validation messages to en.yml
    * moved out some validation messages from models to en.yml

--- app/models/location.rb
+++ app/models/location.rb
@@ -14,12 +14,9 @@ class Location < ApplicationRecord
   has_one :adopter_profile
   has_one :organization_profile
 
-  validates :country, presence: {message: "Please enter a country"},
-    length: {maximum: 50, message: "50 characters maximum"}
-  validates :city_town, presence: {message: "Please enter a city or town"},
-    length: {maximum: 50, message: "50 characters maximum"}
-  validates :province_state, presence: {message: "Please enter a province or state"},
-    length: {maximum: 50, message: "50 characters maximum"}
+  validates :country, presence: {length: {maximum: 50, message: "50 characters maximum"}}
+  validates :city_town, presence: {length: {maximum: 50, message: "50 characters maximum"}}
+  validates :province_state, presence: {length: {maximum: 50, message: "50 characters maximum"}}
 
   # custom error messages for adopter profile validations
   def custom_messages(attribute)
