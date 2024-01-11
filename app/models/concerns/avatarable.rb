1a167b4f334ef93ff0380242b7cbec55a391aa4d
atbalaji <18101543+atbalaji@users.noreply.github.com>
2024-01-11 15:53:07 +0530

    - Fixed standard:fix error introduced

--- app/models/concerns/avatarable.rb
+++ app/models/concerns/avatarable.rb
@@ -4,9 +4,9 @@ module Avatarable
   included do
     has_one_attached :avatar
 
-    validates :avatar, content_type: { in: ["image/png", "image/jpeg"],
-                                      message: "must be PNG or JPEG" },
-      size: { between: 10.kilobyte..1.megabytes,
-             message: "size must be between 10kb and 1Mb" }
+    validates :avatar, content_type: {in: ["image/png", "image/jpeg"],
+                                      message: "must be PNG or JPEG"},
+      size: {between: 10.kilobyte..1.megabytes,
+             message: "size must be between 10kb and 1Mb"}
   end
 end
