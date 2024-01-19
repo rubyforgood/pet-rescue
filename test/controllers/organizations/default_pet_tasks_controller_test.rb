b48164d6fab1d36a86d6d4d9376a3d851ddbe229
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2024-01-19 13:38:26 +0200

    [#395] Staff can CRUD Default Pet Tasks. Fix failing specs

--- test/controllers/organizations/default_pet_tasks_controller_test.rb
+++ test/controllers/organizations/default_pet_tasks_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Organizations::DefaultPetTasksControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = create(:user, :verified_staff)
+    @user = create(:user, :staff_admin)
     @default_pet_task = create(:default_pet_task)
     set_organization(@user.organization)
     @organization = @user.organization
