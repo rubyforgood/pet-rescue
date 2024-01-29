cfb42077c98f4174dee278f5bedea648924a8faf
Erin Claudio <eclaudio@gmail.com>
2024-01-29 14:24:22 -0500

    Update correct migrate command

--- app/models/foster_account.rb
+++ app/models/foster_account.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: foster_accounts
+#
+#  id      :bigint           not null, primary key
+#  user_id :bigint
+#
+# Indexes
+#
+#  index_foster_accounts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class FosterAccount < ApplicationRecord
   belongs_to :user
 end
