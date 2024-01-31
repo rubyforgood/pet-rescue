f5f321f3f57546b552d78cfdae72d4eab407c931
mononoken <ken.maeshima@pm.me>
2024-01-30 21:07:27 -0800

    Create AdoptablePetPolicy tests

---
+++ test/controllers/adoptable_pets_controller_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+require "action_policy/test_helper"
+
+class AdoptablePetsControllerTest < ActionDispatch::IntegrationTest
+  include ActionPolicy::TestHelper
+
+  setup do
+    @pet = create(:pet)
+  end
+
+  test "show is authorized" do
+    assert_authorized_to(:show?, @pet, with: AdoptablePetPolicy) do
+      get adoptable_pet_path(@pet)
+    end
+  end
+end
