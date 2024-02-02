f253c9dfa0a21eede96cdcd9359cf0fad236b429
mononoken <ken.maeshima@pm.me>
2024-02-01 22:12:43 -0800

    Create Authorizable concern

---
+++ app/models/concerns/authorizable.rb
@@ -0,0 +1,31 @@
+ADOPTER_PERMISSIONS = %i[].freeze
+
+STAFF_PERMISSIONS = (
+  ADOPTER_PERMISSIONS + %i[
+    manage_pets
+  ]
+).freeze
+
+ADMIN_PERMISSIONS = (
+  STAFF_PERMISSIONS + %i[]
+).freeze
+
+PERMISSIONS = {
+  adopter: ADOPTER_PERMISSIONS,
+  staff: STAFF_PERMISSIONS,
+  admin: ADMIN_PERMISSIONS
+}.freeze
+
+module Authorizable
+  extend ActiveSupport::Concern
+
+  included do
+    rolify
+  end
+
+  def permission?(name)
+    roles.any? do |role|
+      PERMISSIONS.fetch(role.name.to_sym).include?(name)
+    end
+  end
+end
