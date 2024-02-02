df8a59b794ed7bffbe8e998009f8509d052593be
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-02-02 14:03:14 -0500

    add recurring boolean, next_due_date_in_days integer columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_01_133430) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_02_190002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -242,6 +242,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_01_133430) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "due_date"
+    t.boolean "recurring", default: false
+    t.integer "next_due_date_in_days"
     t.index ["pet_id"], name: "index_tasks_on_pet_id"
   end
 
