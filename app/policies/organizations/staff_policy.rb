fbbe23ccbb362fbd56494b55fbb643c963c475d7
mononoken <ken.maeshima@pm.me>
2024-02-03 02:21:37 -0800

    Start StaffPolicy rules for activation

--- app/policies/organizations/staff_policy.rb
+++ app/policies/organizations/staff_policy.rb
@@ -1,4 +1,16 @@
 class Organizations::StaffPolicy < ApplicationPolicy
+  def deactivate?
+    permission?(:manage_staff)
+  end
+
+  def activate?
+    permission?(:manage_staff)
+  end
+
+  def update_activation?
+    permission?(:manage_staff)
+  end
+
   def index?
     permission?(:manage_staff)
   end
--- test/policies/organizations/staff_policy_test.rb
+++ test/policies/organizations/staff_policy_test.rb
@@ -2,23 +2,151 @@ require "test_helper"
 
 # See https://actionpolicy.evilmartians.io/#/testing?id=testing-policies
 class Organizations::StaffPolicyTest < ActiveSupport::TestCase
-  def setup
+  setup do
+    @staff = create(:staff_account)
     @policy = -> { Organizations::StaffPolicy.new(@staff, user: @user) }
   end
 
   context "#deactivate?" do
+    setup do
+      @action = -> { @policy.call.apply(:deactivate?) }
+    end
+
+    context "when user is nil" do
+      setup do
+        @user = nil
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is adopter" do
+      setup do
+        @user = create(:user, :adopter_without_profile)
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is staff" do
+      setup do
+        @user = create(:user, :activated_staff)
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is admin" do
+      setup do
+        @user = create(:user, :staff_admin)
+      end
+
+      should "return true" do
+        assert_equal @action.call, true
+      end
+    end
   end
 
   context "#activate?" do
+    setup do
+      @action = -> { @policy.call.apply(:activate?) }
+    end
+
+    context "when user is nil" do
+      setup do
+        @user = nil
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is adopter" do
+      setup do
+        @user = create(:user, :adopter_without_profile)
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is staff" do
+      setup do
+        @user = create(:user, :activated_staff)
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is admin" do
+      setup do
+        @user = create(:user, :staff_admin)
+      end
+
+      should "return true" do
+        assert_equal @action.call, true
+      end
+    end
   end
 
   context "#update_activation?" do
+    setup do
+      @action = -> { @policy.call.apply(:update_activation?) }
+    end
+
+    context "when user is nil" do
+      setup do
+        @user = nil
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is adopter" do
+      setup do
+        @user = create(:user, :adopter_without_profile)
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is staff" do
+      setup do
+        @user = create(:user, :activated_staff)
+      end
+
+      should "return false" do
+        assert_equal @action.call, false
+      end
+    end
+
+    context "when user is admin" do
+      setup do
+        @user = create(:user, :staff_admin)
+      end
+
+      should "return true" do
+        assert_equal @action.call, true
+      end
+    end
   end
 
   context "#index?" do
     setup do
-      @staff = create(:staff_account)
-
       @action = -> { @policy.call.apply(:index?) }
     end
 
