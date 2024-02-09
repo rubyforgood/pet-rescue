3ab0898e9fb98a3bd2092d7b6e52f33bc1133ce2
mononoken <ken.maeshima@pm.me>
2024-02-08 23:04:38 -0800

    Sort authorization tests into separate context in InvitationsControllerTest and StaffControllerTest

--- test/controllers/organizations/invitations_controller_test.rb
+++ test/controllers/organizations/invitations_controller_test.rb
@@ -2,46 +2,47 @@ require "test_helper"
 require "action_policy/test_helper"
 
 class Organizations::InvitationControllerTest < ActionDispatch::IntegrationTest
-  include ActionPolicy::TestHelper
+  context "authorization" do
+    include ActionPolicy::TestHelper
 
-  setup do
-    user = create(:user, :staff_admin)
-    sign_in user
+    setup do
+      @organization = ActsAsTenant.current_tenant
+      user = create(:user, :staff_admin)
+      sign_in user
+    end
 
-    @organization = user.organization
-  end
-
-  context "#new" do
-    should "be authorized" do
-      assert_authorized_to(
-        :create?, StaffAccount,
-        context: {organization: @organization},
-        with: Organizations::InvitationPolicy
-      ) do
-        get new_user_invitation_url
+    context "#new" do
+      should "be authorized" do
+        assert_authorized_to(
+          :create?, StaffAccount,
+          context: {organization: @organization},
+          with: Organizations::InvitationPolicy
+        ) do
+          get new_user_invitation_url
+        end
       end
     end
-  end
 
-  context "#create" do
-    setup do
-      @params = {
-        user: {
-          first_name: "John",
-          last_name: "Doe",
-          email: "john@example.com",
-          roles: "admin"
+    context "#create" do
+      setup do
+        @params = {
+          user: {
+            first_name: "John",
+            last_name: "Doe",
+            email: "john@example.com",
+            roles: "admin"
+          }
         }
-      }
-    end
+      end
 
-    should "be authorized" do
-      assert_authorized_to(
-        :create?, StaffAccount,
-        context: {organization: @organization},
-        with: Organizations::InvitationPolicy
-      ) do
-        post user_invitation_url, params: @params
+      should "be authorized" do
+        assert_authorized_to(
+          :create?, StaffAccount,
+          context: {organization: @organization},
+          with: Organizations::InvitationPolicy
+        ) do
+          post user_invitation_url, params: @params
+        end
       end
     end
   end
--- test/controllers/organizations/staff_controller_test.rb
+++ test/controllers/organizations/staff_controller_test.rb
@@ -2,75 +2,77 @@ require "test_helper"
 require "action_policy/test_helper"
 
 class Organizations::StaffControllerTest < ActionDispatch::IntegrationTest
-  include ActionPolicy::TestHelper
-
   setup do
     @organization = ActsAsTenant.current_tenant
     @staff = create(:staff_account)
   end
 
-  context "#deactivate" do
-    should "be authorized" do
-      assert_authorized_to(
-        :activate?, @staff, with: Organizations::StaffPolicy
-      ) do
-        post staff_deactivate_url(@staff)
-      end
-    end
-  end
+  context "authorization" do
+    include ActionPolicy::TestHelper
 
-  context "#activate" do
-    should "be authorized" do
-      assert_authorized_to(
-        :activate?, @staff, with: Organizations::StaffPolicy
-      ) do
-        post staff_activate_url(@staff)
-      end
-    end
-  end
-
-  context "#update_activation" do
-    should "be authorized" do
-      assert_authorized_to(
-        :activate?, @staff, with: Organizations::StaffPolicy
-      ) do
-        post staff_update_activation_url(@staff)
-      end
-    end
-  end
-
-  context "#index" do
-    should "be authorized" do
-      assert_authorized_to(
-        :index?, StaffAccount,
-        context: {organization: @organization},
-        with: Organizations::StaffPolicy
-      ) do
-        get staff_index_url
+    context "#deactivate" do
+      should "be authorized" do
+        assert_authorized_to(
+          :activate?, @staff, with: Organizations::StaffPolicy
+        ) do
+          post staff_deactivate_url(@staff)
+        end
       end
     end
 
-    context "when user is authorized" do
-      setup do
-        user = create(:user, :staff_admin)
-        set_organization(@organization)
-        sign_in user
+    context "#activate" do
+      should "be authorized" do
+        assert_authorized_to(
+          :activate?, @staff, with: Organizations::StaffPolicy
+        ) do
+          post staff_activate_url(@staff)
+        end
       end
+    end
 
-      should "have authorized scope" do
-        assert_have_authorized_scope(
-          type: :active_record_relation, with: Organizations::StaffPolicy
+    context "#update_activation" do
+      should "be authorized" do
+        assert_authorized_to(
+          :activate?, @staff, with: Organizations::StaffPolicy
+        ) do
+          post staff_update_activation_url(@staff)
+        end
+      end
+    end
+
+    context "#index" do
+      should "be authorized" do
+        assert_authorized_to(
+          :index?, StaffAccount,
+          context: {organization: @organization},
+          with: Organizations::StaffPolicy
         ) do
           get staff_index_url
+        end
+      end
+
+      context "when user is authorized" do
+        setup do
+          user = create(:user, :staff_admin)
+          set_organization(@organization)
+          sign_in user
+        end
+
+        should "have authorized scope" do
+          assert_have_authorized_scope(
+            type: :active_record_relation, with: Organizations::StaffPolicy
+          ) do
+            get staff_index_url
+          end
         end
       end
     end
   end
 
   test "update activation should respond with turbo_stream when toggled on staff page" do
-    @user = create(:user, :staff_admin)
-    set_organization(@user.organization)
-    sign_in @user
+    user = create(:user, :staff_admin)
+    set_organization(user.organization)
+    sign_in user
 
     post staff_update_activation_url(@staff), as: :turbo_stream
 
