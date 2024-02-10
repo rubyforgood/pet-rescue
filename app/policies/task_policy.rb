c00ebcb96a3bd4af0e0a0379d23d22549b5a562d
mononoken <ken.maeshima@pm.me>
2024-02-09 22:59:14 -0800

    Create TaskPolicy with tests

---
+++ app/policies/task_policy.rb
@@ -0,0 +1,23 @@
+class TaskPolicy < ApplicationPolicy
+  authorize :pet, optional: true
+
+  pre_check :verify_organization!
+  pre_check :verify_active_staff!
+  pre_check :verify_pet!
+
+  alias_rule :create?, :new?, to: :manage?
+
+  def manage?
+    permission?(:manage_tasks)
+  end
+
+  private
+
+  def pet
+    @pet || record.pet
+  end
+
+  def verify_pet!
+    deny! unless allowed_to?(:manage?, pet, with: Organizations::PetPolicy)
+  end
+end
