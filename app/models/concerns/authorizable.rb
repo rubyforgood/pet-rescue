8bc1cadaa281953a243959070d8aef54e6109b68
mononoken <ken.maeshima@pm.me>
2024-02-12 20:50:50 -0800

    Move permissions constants into private

--- app/models/concerns/authorizable.rb
+++ app/models/concerns/authorizable.rb
@@ -1,26 +1,3 @@
-ADOPTER_PERMISSIONS = %i[].freeze
-
-STAFF_PERMISSIONS = (
-  ADOPTER_PERMISSIONS + %i[
-    manage_pets
-    manage_tasks
-  ]
-).freeze
-
-ADMIN_PERMISSIONS = (
-  STAFF_PERMISSIONS + %i[
-    activate_staff
-    invite_staff
-    manage_staff
-  ]
-).freeze
-
-PERMISSIONS = {
-  adopter: ADOPTER_PERMISSIONS,
-  staff: STAFF_PERMISSIONS,
-  admin: ADMIN_PERMISSIONS
-}.freeze
-
 module Authorizable
   extend ActiveSupport::Concern
 
@@ -39,4 +16,27 @@ module Authorizable
       permissions.concat(PERMISSIONS.fetch(role.name.to_sym))
     end
   end
+
+  ADOPTER_PERMISSIONS = %i[].freeze
+
+  STAFF_PERMISSIONS = (
+    ADOPTER_PERMISSIONS + %i[
+      manage_pets
+      manage_tasks
+    ]
+  ).freeze
+
+  ADMIN_PERMISSIONS = (
+    STAFF_PERMISSIONS + %i[
+      activate_staff
+      invite_staff
+      manage_staff
+    ]
+  ).freeze
+
+  PERMISSIONS = {
+    adopter: ADOPTER_PERMISSIONS,
+    staff: STAFF_PERMISSIONS,
+    admin: ADMIN_PERMISSIONS
+  }.freeze
 end
