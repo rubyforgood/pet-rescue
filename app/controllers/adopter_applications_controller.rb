6df4e11ef176bad810b88c38f03d5481452e1784
Ken Maeshima <81536479+mononoken@users.noreply.github.com>
2024-03-01 20:25:26 -0800

    Refactor AdopterApplications routes to more standard Rails convention and change post/patch buttons to forms (#475)

--- app/controllers/adopter_applications_controller.rb
+++ app/controllers/adopter_applications_controller.rb
@@ -12,27 +12,27 @@ class AdopterApplicationsController < ApplicationController
     @application = AdopterApplication.new(application_params)
 
     if @application.save
-      redirect_to adoptable_pet_path(params[:application][:pet_id]),
+      redirect_to adoptable_pet_path(@application.pet),
         notice: "Application submitted! #{MessagesHelper.affirmations.sample}"
 
       # mailer
-      @pet = Pet.find(params[:application][:pet_id])
+      @pet = @application.pet
       @org_staff = User.organization_staff(@pet.organization_id)
       StaffApplicationNotificationMailer.with(pet: @pet,
         organization_staff: @org_staff)
         .new_adoption_application.deliver_now
     else
-      redirect_to adoptable_pet_path(params[:application][:pet_id]),
+      redirect_to adoptable_pet_path(@application.pet),
         alert: "Error. Please try again."
     end
   end
 
   # update :status to 'withdrawn' or :profile_show to false
   def update
-    @application = AdopterApplication.find(params[:application][:id])
+    @application = AdopterApplication.find(params[:id])
 
     if @application.update(application_params)
-      redirect_to my_applications_path
+      redirect_to adopter_applications_path
     else
       redirect_to profile_path, alert: "Error."
     end
@@ -41,19 +41,20 @@ class AdopterApplicationsController < ApplicationController
   private
 
   def application_params
-    params.require(:application).permit(:id,
+    params.require(:adopter_application).permit(
       :pet_id,
       :adopter_account_id,
       :status,
-      :profile_show)
+      :profile_show
+    )
   end
 
   def check_pet_app_status
-    pet = Pet.find(params[:application][:pet_id])
+    pet = Pet.find(params[:adopter_application][:pet_id])
 
     return if pet.application_paused == false
 
-    redirect_to adoptable_pet_path(params[:application][:pet_id]),
+    redirect_to adoptable_pet_path(params[:adopter_application][:pet_id]),
       alert: "Applications are paused for this pet"
   end
 end
