8523e64020a2dfed24df09a752b562ff959ae2c4
mononoken <ken.maeshima@pm.me>
2024-03-09 16:48:14 -0800

    Refactor MatchesController to use Rails naming conventions such as destroy instead of delete

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,27 +1,28 @@
 class MatchesController < ApplicationController
   before_action :active_staff, :same_organization?
 
+  before_action :set_pet, only: %i[create]
+  before_action :set_match, only: %i[destroy]
+
   def create
-    @pet = Pet.find(adoption_params[:pet_id])
-    @match = Match.new(adoption_params.merge(organization_id: @pet.organization_id))
+    @match = Match.new(match_params.merge(
+      organization_id: @pet.organization_id
+    ))
 
     if @match.save
       set_statuses_to_adoption_made
       redirect_back_or_to dashboard_index_path, notice: "Pet successfully adopted."
     else
       redirect_back_or_to dashboard_index_path, alert: "Error."
-
     end
   end
-
-  def delete
-    @match = Match.find(params[:match_id])
 
     @successful_application = @match.adopter_account
       .adopter_applications
       .where(pet_id: @match.pet_id)[0]
     AdopterApplication.set_status_to_withdrawn(@successful_application)
 
+  def destroy
     if @match.destroy
       redirect_to pets_path, notice: "Adoption reverted & application set to 'Withdrawn'"
     else
@@ -31,8 +32,8 @@ class MatchesController < ApplicationController
 
   private
 
-  def adoption_params
-    params.permit(:pet_id, :adopter_account_id, :match_id)
+  def match_params
+    params.permit(:pet_id, :adopter_account_id)
   end
 
   # set status on all applications for a pet
@@ -44,12 +45,18 @@ class MatchesController < ApplicationController
         app.save
       end
     end
+  def set_pet
+    @pet = Pet.find(match_params[:pet_id])
+  end
+
+  def set_match
+    @match = Match.find(params[:id])
   end
 
   def get_pet_id
     return params[:pet_id] if params[:pet_id]
 
-    Match.find(params[:match_id]).pet_id
+    Match.find(params[:id]).pet_id
   end
 
   # staff and pet in the same org?
