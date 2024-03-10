a91d50cad3cfe81b33539c59b13f8399ee5bf0eb
Ben R <robinsonbena@gmail.com>
2024-03-10 15:52:10 -0700

    Add nested attr for adopter account in new user registration form (which is only used by adopter sign ups) as it was missing. Now when a user signs up via this form the associated adopter account is created.

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,7 @@ class User < ApplicationRecord
   has_one :staff_account, dependent: :destroy
   has_one :adopter_account, dependent: :destroy
 
-  accepts_nested_attributes_for :adopter_account, :staff_account
+  accepts_nested_attributes_for :adopter_account
 
   # get user accounts for staff in a given organization
   def self.organization_staff(org_id)
