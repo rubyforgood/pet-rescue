eda30ee5aa76df301f73143a6eea5d9afb25c7c9
phonghpham <9318603+phonghpham@users.noreply.github.com>
2024-03-14 15:08:26 -0500

    Add controller test for page text

---
+++ test/controllers/organizations/page_texts_controller_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class Organizations::PageTextsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @org = ActsAsTenant.current_tenant
+    admin = create(:user, :staff_admin, organization: @org)
+    sign_in admin
+    @page_text = create(:page_text, organization: @org)
+  end
+
+  context "GET #edit" do
+    should "get edit page" do
+      get edit_page_text_path(@page_text)
+      assert_response :success
+    end
+  end
+
+  context "PATCH #update" do
+    should "update page text" do
+      patch page_text_path(@page_text), params: { page_text: { hero: "Super Dog", about: "canine caped crusader" } }
+      assert_redirected_to dashboard_path
+      @page_text.reload
+      assert_equal "Super Dog", @page_text.hero
+      assert_equal "canine caped crusader", @page_text.about
+    end
+  end
+end
