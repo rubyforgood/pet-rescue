c222a9a450420e1667eac4a546677d2b26acc9a2
Erin Claudio <eclaudio@gmail.com>
2024-03-14 13:38:30 -0400

    Standard fix

--- db/schema.rb
+++ db/schema.rb
@@ -122,6 +122,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_14_171529) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "foster_accounts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foster_accounts_on_user_id"
+  end
+
   create_table "locations", force: :cascade do |t|
     t.string "country"
     t.string "city_town"
