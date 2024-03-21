ae1d845916387edc432199294a80a2981583f942
Ken Maeshima <81536479+mononoken@users.noreply.github.com>
2024-03-21 15:17:12 -0700

    Change 422 to correct status name, Unprocessable Content (#575)

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
   end
 
   match "/404", to: "errors#not_found", via: :all
-  match "/422", to: "errors#restricted_access", via: :all
+  match "/422", to: "errors#unprocessable_content", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
 
   root "root#index"
