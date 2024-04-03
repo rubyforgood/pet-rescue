d911ae95e58e91312f0ae1683246462f2e089bdb
Bryan Witherspoon <765993+uoodsq@users.noreply.github.com>
2024-04-03 19:23:07 +0000

    Fix some translations in QuestionsController

--- app/controllers/organizations/questions_controller.rb
+++ app/controllers/organizations/questions_controller.rb
@@ -1,75 +1,73 @@
-# frozen_string_literal: true
+class Organizations::QuestionsController < ApplicationController
+  before_action :context_authorize!
+  before_action :set_form
+  before_action :set_question, only: %i[show edit update destroy]
 
-module Organizations
-  class QuestionsController < ApplicationController
-    before_action :context_authorize!
-    before_action :set_form
-    before_action :set_question, only: %i[show edit update destroy]
+  layout 'dashboard'
 
-    layout 'dashboard'
+  def index
+    @questions = authorized_scope(@form.questions)
+  end
 
-    def index
-      @questions = authorized_scope(@form.questions)
+  def new
+    @question = @form.questions.new
+  end
+
+  def create
+    @question = @form.questions.new(question_params)
+
+    if @question.save
+      redirect_to form_path(@form), notice: t('.saved')
+    else
+      flash.now[:alert] = t('.error')
+
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-      @question = @form.questions.new
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to form_path(@form), notice: t('.updated')
+    else
+      flash.now[:alert] = t('.error')
+
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def create
-      @question = @form.questions.new(question_params)
+  def destroy
+    @question.destroy
 
-      if @question.save
-        redirect_to form_path(@form), notice: t('.saved')
-      else
-        flash.now[:alert] = t('.error')
+    redirect_to form_path(@form), notice: t('.destroyed')
+  end
 
-        render :new, status: :unprocessable_entity
-      end
-    end
+  private
 
-    def edit; end
+  def organization
+    current_user.organization
+  end
 
-    def update
-      if @question.update(question_params)
-        redirect_to form_path(@form), notice: t('.updated')
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
+  def set_form
+    @form = organization.forms.find(params[:form_id])
+    authorize! @form
+  rescue ActiveRecord::RecordNotFound
+    redirect_to forms_path, alert: t('organizations.forms.not_found')
+  end
 
-    def destroy
-      @question.destroy
+  def set_question
+    @question = @form.questions.find(params[:id])
+    authorize! @question
+  rescue ActiveRecord::RecordNotFound
+    redirect_to form_path(@form), alert: t('.not_found')
+  end
 
-      redirect_to form_path(@form), notice: t('.destroyed')
-    end
+  def question_params
+    params.require(:question).permit(:name, :description, :label, :help_text, :required, :input_type, :options)
+  end
 
-    private
-
-    def organization
-      current_user.organization
-    end
-
-    def set_form
-      @form = organization.forms.find(params[:form_id])
-      authorize! @form
-    rescue ActiveRecord::RecordNotFound
-      redirect_to forms_path, alert: t('.form_not_found')
-    end
-
-    def set_question
-      @question = @form.questions.find(params[:id])
-      authorize! @question
-    rescue ActiveRecord::RecordNotFound
-      redirect_to form_path(@form), alert: t('.not_found')
-    end
-
-    def question_params
-      params.require(:question).permit(:name, :description, :label, :help_text, :required, :input_type, :options)
-    end
-
-    def context_authorize!
-      authorize! Question, context: { organization: Current.organization }
-    end
+  def context_authorize!
+    authorize! Question, context: { organization: Current.organization }
   end
 end
