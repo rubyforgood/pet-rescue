4216e87b3be49530d2e37ff511ec73dfe8ac05d4
Alisa <82609260+MooseCowBear@users.noreply.github.com>
2024-04-04 17:03:11 -0400

    590 parameterize contact mailer (#607)
    * commit annotation change
    
    * update contacts create route to use post and update url on contacts form
    
    * add tests for contacts
    
    * replace hardcoded url, add instance variable for org name to use in mailer
    
    * replace current_tenant because it was coming up nil
    
    * add scope to contact form and update controller action to use stong params
    
    * update tests to use correct params format
    
    * fix test desciptions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,15 +6,11 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(name: params[:name],
-      email: params[:email],
-      message: params[:message])
+    @contact = Contact.new(contacts_params)
 
     if @contact.valid?
-      ContactsMailer.with(name: params[:name],
-        email: params[:email],
-        message: params[:message])
-        .send_message(current_tenant.slug).deliver_now
+      ContactsMailer.with(contacts_params)
+        .send_message(Current.organization.slug).deliver_now
       redirect_to root_path, notice: "Message sent!"
     else
       render :new, status: :unprocessable_entity
