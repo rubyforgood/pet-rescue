8eb8bfcd2f4abdf77aaf661048ed9cbc0fe1fdf6
Vaughn Weiss <vaughn.weiss@gmail.com>
2024-04-11 17:57:06 -0600

    Display feature pets on org home page [558]

--- test/controllers/organizations/home_controller_test.rb
+++ test/controllers/organizations/home_controller_test.rb
@@ -2,15 +2,23 @@ require "test_helper"
 
 class Organizations::HomeControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @organization = create(:organization)
+    @pets = create_list(:pet, 4, :with_image)
+    set_organization(@pets.first.organization)
   end
 
   context "GET #index" do
     should "render index template" do
-      get home_index_path(script_name: "/#{@organization.slug}")
+      get home_index_path
 
       assert_response :success
-      assert_select "title", text: "#{@organization.name} | \"Pet Rescue\""
+      assert_select "title", text: "#{ActsAsTenant.current_tenant.name} | \"Pet Rescue\""
+    end
+
+    should "render four random organization pets" do
+      get home_index_path
+
+      assert_response :success
+      @pets.each { |pet| assert_select "p", "Adopt #{pet.name}" }
     end
   end
 end
