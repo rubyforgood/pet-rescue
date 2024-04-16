9cea20d6287fe15d893a4699a166f66ccc9c3949
Erin Claudio <eclaudio@gmail.com>
2024-04-16 19:41:07 -0400

    Update adjusted one test to pass

--- test/integration/adoption_application_reviews_test.rb
+++ test/integration/adoption_application_reviews_test.rb
@@ -8,66 +8,70 @@ class AdoptionApplicationReviewsTest < ActionDispatch::IntegrationTest
     @withdrawn_app = create(:adopter_application, :withdrawn)
     @successful_applicant_app = create(:adopter_application, status: :successful_applicant)
     @adoption_made_app = create(:adopter_application, status: :adoption_made)
+    @organization = create(:organization)
+    @page_text = create(:page_text, organization: @organization)
+    @organization.page_text = @page_text
+    Current.organization = @organization
   end
 
-  context "non-staff" do
-    should "not see any applications" do
+  context 'non-staff' do
+    should 'not see any applications' do # HERE
       get adoption_application_reviews_path
 
       assert_response :redirect
       follow_redirect!
       follow_redirect!
-      assert_equal "You are not authorized to perform this action.", flash[:alert]
+      assert_equal 'You are not authorized to perform this action.', flash[:alert]
     end
   end
 
-  context "active staff" do
+  context 'active staff' do
     setup do
       sign_in create(:staff)
     end
 
-    should "see all applications" do
+    should 'see all applications' do
       get adoption_application_reviews_path
 
       assert_response :success
       AdopterApplication.all.each { |application| verify_application_elements application }
     end
 
-    should "be able to change the application status" do
+    should 'be able to change the application status' do
       patch adoption_application_review_path(@awaiting_review_app.id),
-        params: {adopter_application: {status: :under_review}},
-        headers: {"HTTP_REFERER" => "example.com"}
+            params: { adopter_application: { status: :under_review } },
+            headers: { 'HTTP_REFERER' => 'example.com' }
 
       assert_response :redirect
       follow_redirect!
       @awaiting_review_app.reload
-      assert_equal "under_review", @awaiting_review_app.status
+      assert_equal 'under_review', @awaiting_review_app.status
     end
 
-    should "be able to add a note to an application" do
+    should 'be able to add a note to an application' do
       patch adoption_application_review_path(@under_review_app.id),
-        params: {adopter_application: {notes: "some notes"}},
-        headers: {"HTTP_REFERER" => "example.com"}
+            params: { adopter_application: { notes: 'some notes' } },
+            headers: { 'HTTP_REFERER' => 'example.com' }
 
       assert_response :redirect
       follow_redirect!
 
       @under_review_app.reload
-      assert_equal("some notes", @under_review_app.notes)
+      assert_equal('some notes', @under_review_app.notes)
     end
 
-    context "deactivated staff" do
+    context 'deactivated staff' do
       setup do
         sign_in create(:staff_account, :deactivated).user
       end
 
-      should_eventually "not see any applications" do
+      should_eventually 'not see any applications' do
         get adoption_application_reviews_path
 
         assert_response :redirect
         follow_redirect!
         follow_redirect!
-        assert_equal "Unauthorized action.", flash[:alert]
+        assert_equal 'Unauthorized action.', flash[:alert]
       end
     end
   end
@@ -75,8 +79,8 @@ class AdoptionApplicationReviewsTest < ActionDispatch::IntegrationTest
   def verify_application_elements(application)
     assert_select "tr[id='adopter_application_#{application.id}']" do
       adopter = application.adopter_foster_account.user
-      assert_select "a", text: "#{adopter.last_name}, #{adopter.first_name}"
-      assert_select "button", text: application.status.titleize
+      assert_select 'a', text: "#{adopter.last_name}, #{adopter.first_name}"
+      assert_select 'button', text: application.status.titleize
     end
   end
 end
