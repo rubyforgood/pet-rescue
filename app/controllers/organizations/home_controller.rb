52c51b1221d1d9503f4d1f31ea962e9c99464549
Vaughn Weiss <vaughn.weiss@gmail.com>
2024-04-20 23:14:25 -0600

    Remove use of constant

--- app/controllers/organizations/home_controller.rb
+++ app/controllers/organizations/home_controller.rb
@@ -1,9 +1,9 @@
 class Organizations::HomeController < Organizations::BaseController
   skip_verify_authorized only: %i[index]
 
-  SAMPLE_SIZE = 4
-
   def index
-    @pets = Pet.includes(images_attachments: :blob).where(organization: Current.organization).sample(SAMPLE_SIZE)
+    @pets = Pet.includes(images_attachments: :blob)
+               .where(organization: Current.organization)
+               .sample(4)
   end
 end
