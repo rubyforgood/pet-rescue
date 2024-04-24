fb1a63455408f15f5068d91e910d3707347c7932
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-04-24 06:51:36 -0400

    Change seeds

--- db/seeds/01_alta.rb
+++ db/seeds/01_alta.rb
@@ -285,7 +285,7 @@ ActsAsTenant.with_tenant(@organization) do
   10.times do
     Faq.create!(
       question: Faker::Lorem.question(word_count: 4, random_words_to_add: 10),
-      answer: Faker::Lorem.sentence(word_count: 1, random_words_to_add: 20)
+      answer: Faker::Lorem.sentence(word_count: 1, random_words_to_add: 50)
     )
   end
 end
