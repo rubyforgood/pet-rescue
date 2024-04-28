5b24661321cd510f51e8df9777bb28e94edc48e4
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2024-04-28 19:08:53 -0400

    feat: replace dummy contact info with org info
    Adds the organization contact information to the footer partial

--- test/system/home_page_test.rb
+++ test/system/home_page_test.rb
@@ -8,9 +8,17 @@ class HomePageTest < ApplicationSystemTestCase
   end
 
   test "renders custom hero and about text from PageText or default text" do
-    PageText.create(hero: "Super Pets for a good paws", about: "All about us")
+    create(:page_text, hero: "Super Pets for a good paws", about: "All about us")
+
     visit home_index_path
     assert_text "Super Pets for a good paws"
     assert_text "All about us"
+
+    # Should render the organization location information
+    assert_text @organization.location.city_town
+    assert_text @organization.location.province_state
+    assert_text @organization.location.country
+    assert_text @organization.profile.email
+    assert_text @organization.profile.phone_number
   end
 end
