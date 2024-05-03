a0387f150adbd6238de230a8db8af6dd97aa614a
mononoken <ken.maeshima@pm.me>
2024-05-03 16:31:54 -0700

    Add tests for StaffInvitationPolicy and StaffInvitationsController

---
+++ test/controllers/organizations/staff_invitations_controller_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+require "action_policy/test_helper"
+
+class Organizations::StaffInvitationsControllerTest < ActionDispatch::IntegrationTest
+  context "authorization" do
+    include ActionPolicy::TestHelper
+
+    setup do
+      @organization = ActsAsTenant.current_tenant
+      user = create(:staff_admin)
+      sign_in user
+    end
+
+    context "#new" do
+      should "be authorized" do
+        assert_authorized_to(
+          :create?, User,
+          context: {organization: @organization},
+          with: Organizations::StaffInvitationPolicy
+        ) do
+          get new_staff_invitation_url
+        end
+      end
+    end
+  end
+end
