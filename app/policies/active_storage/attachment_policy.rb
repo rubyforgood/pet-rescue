70403fa0ae70cd7d144554a37de68737bc4c9997
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-05-04 07:37:47 -0400

    Add purge_avatar? to policy

--- app/policies/active_storage/attachment_policy.rb
+++ app/policies/active_storage/attachment_policy.rb
@@ -1,9 +1,13 @@
 class ActiveStorage::AttachmentPolicy < ApplicationPolicy
   pre_check :verify_organization!
-  pre_check :verify_active_staff!
+  pre_check :verify_active_staff!, only: [:purge?]
 
   def purge?
     permission?(:purge_attachments)
+  end
+
+  def purge_avatar?
+    permission?(:purge_avatar)
   end
 
   private
