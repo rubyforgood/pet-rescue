5b33e77e7b2277996bf83d85a0726031c64448c8
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-05-04 09:49:03 -0400

    add authorization tests

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,11 +1,21 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "john-#{n}@example.com" }
-    password { "123456" }
-    encrypted_password { Devise::Encryptor.digest(User, "123456") }
+    password { '123456' }
+    encrypted_password { Devise::Encryptor.digest(User, '123456') }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     tos_agreement { true }
+
+    trait :with_avatar do
+      after(:build) do |user|
+        user.avatar.attach(
+          io: File.open(Rails.root.join('test', 'fixtures', 'files', 'test.png')),
+          filename: 'test.png',
+          content_type: 'image/png'
+        )
+      end
+    end
 
     factory :adopter do
       adopter_foster_account do
