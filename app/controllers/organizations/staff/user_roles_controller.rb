62a3f5d74df9c77ed6695b84268c54c4ebcafbb5
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-05-12 17:14:27 -0400

    refactor controller

--- app/controllers/organizations/staff/user_roles_controller.rb
+++ app/controllers/organizations/staff/user_roles_controller.rb
@@ -3,35 +3,13 @@ class Organizations::Staff::UserRolesController < Organizations::BaseController
 
   def to_staff
     ActiveRecord::Base.transaction do
-      if @user.add_role(:staff, Current.organization) && @user.remove_role(:admin, Current.organization)
-        respond_to do |format|
-          format.html { redirect_to request.referrer, notice: "Account changed to Staff" }
-          format.turbo_stream { flash.now[:notice] = "Account changed to Staff" }
-        end
-      else
-        respond_to do |format|
-          format.html { redirect_to request.referrer, alert: "Error changing role" }
-          format.turbo_stream { flash.now[:alert] = "Error changing role" }
-        end
-        raise ActiveRecord::Rollback
-      end
+      change_role(@user, :admin, :staff)
     end
   end
 
   def to_admin
     ActiveRecord::Base.transaction do
-      if @user.add_role(:admin, Current.organization) && @user.remove_role(:staff, Current.organization)
-        respond_to do |format|
-          format.html { redirect_to request.referrer, notice: "Account changed to Admin" }
-          format.turbo_stream { flash.now[:notice] = "Account changed to Admin" }
-        end
-      else
-        respond_to do |format|
-          format.html { redirect_to request.referrer, alert: "Error changing role" }
-          format.turbo_stream { flash.now[:alert] = "Error changing role" }
-        end
-        raise ActiveRecord::Rollback
-      end
+      change_role(@user, :staff, :admin)
     end
   end
 
@@ -41,5 +19,23 @@ class Organizations::Staff::UserRolesController < Organizations::BaseController
     @user = User.find(params[:id])
     authorize! @user, with: Organizations::UserRolesPolicy, to: :change_role?,
       context: {organization: Current.organization}
+  end
+
+  def change_role(user, from_role, to_role)
+    user.add_role(to_role, Current.organization)
+    user.remove_role(from_role, Current.organization)
+
+    if user.only_has_role?(to_role, Current.organization)
+      respond_to do |format|
+        format.html { redirect_to request.referrer, notice: "Account changed to #{to_role}" }
+        format.turbo_stream { flash.now[:notice] = "Account changed to #{to_role}" }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to request.referrer, alert: "Error changing role" }
+        format.turbo_stream { flash.now[:alert] = "Error changing role" }
+      end
+      raise ActiveRecord::Rollback
+    end
   end
 end
