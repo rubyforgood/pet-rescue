dbc0c3ed12fb18ae259a236cb7dcf9c72175ca32
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-05-14 19:02:14 -0400

    move transaction

--- app/controllers/organizations/staff/user_roles_controller.rb
+++ app/controllers/organizations/staff/user_roles_controller.rb
@@ -2,15 +2,11 @@ class Organizations::Staff::UserRolesController < Organizations::BaseController
   before_action :set_user
 
   def to_staff
-    ActiveRecord::Base.transaction do
-      change_role(@user, :admin, :staff)
-    end
+    change_role(@user, :admin, :staff)
   end
 
   def to_admin
-    ActiveRecord::Base.transaction do
-      change_role(@user, :staff, :admin)
-    end
+    change_role(@user, :staff, :admin)
   end
 
   private
@@ -21,21 +17,19 @@ class Organizations::Staff::UserRolesController < Organizations::BaseController
       context: {organization: Current.organization}
   end
 
-  def change_role(user, from_role, to_role)
-    user.add_role(to_role, Current.organization)
-    user.remove_role(from_role, Current.organization)
-
-    if user.only_has_role?(to_role, Current.organization)
+  def change_role(user, previous, new)
+    ActiveRecord::Base.transaction do
+      user.remove_role(previous, user.organization)
+      user.add_role(new, user.organization)
       respond_to do |format|
-        format.html { redirect_to request.referrer, notice: "Account changed to #{to_role}" }
-        format.turbo_stream { flash.now[:notice] = "Account changed to #{to_role}" }
+        format.html { redirect_to request.referrer, notice: "Account changed to #{new.capitalize}" }
+        format.turbo_stream { flash.now[:notice] = "Account changed to #{new.capitalize}" }
       end
-    else
-      respond_to do |format|
-        format.html { redirect_to request.referrer, alert: "Error changing role" }
-        format.turbo_stream { flash.now[:alert] = "Error changing role" }
-      end
-      raise ActiveRecord::Rollback
+    end
+  rescue
+    respond_to do |format|
+      format.html { redirect_to request.referrer, alert: "Error changing role" }
+      format.turbo_stream { flash.now[:alert] = "Error changing role" }
     end
   end
 end
