d69a739c5aa613b2eb0b32558f779397b1092ef0
atbalaji <18101543+atbalaji@users.noreply.github.com>
2024-05-16 02:28:38 +0530

    Addressed comments

--- app/controllers/organizations/staff/adoption_application_reviews_controller.rb
+++ app/controllers/organizations/staff/adoption_application_reviews_controller.rb
@@ -21,7 +21,7 @@ class Organizations::Staff::AdoptionApplicationReviewsController < Organizations
       @pets_with_applications = filter_by_application_status(@pets_with_applications, status_filter)
     end
 
-    @pagy, @pets_with_applications = pagy(@pets_with_applications, items: AdopterApplication::REVIEWS_PER_PAGE)
+    @pagy, @pets_with_applications = pagy(@pets_with_applications, items: 10)
   end
 
   def edit
