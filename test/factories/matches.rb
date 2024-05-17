7f1db2256134fcd977858602fd35c64effdcf845
mononoken <ken.maeshima@pm.me>
2024-05-17 15:07:57 -0700

    Add match type to Match factory to fix it

--- test/factories/matches.rb
+++ test/factories/matches.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :match do
     pet { association :pet }
     adopter_foster_account { association :adopter_foster_account, :with_profile }
+    match_type { :adoption }
   end
 end
