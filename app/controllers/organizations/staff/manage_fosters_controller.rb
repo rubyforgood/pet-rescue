5a81f5b198d3e6ef1866796e98ed8b731c151bdc
mononoken <ken.maeshima@pm.me>
2024-05-17 22:53:35 -0700

    Get fostered pets with ransack and Match's fosters scope

--- app/controllers/organizations/staff/manage_fosters_controller.rb
+++ app/controllers/organizations/staff/manage_fosters_controller.rb
@@ -4,5 +4,8 @@ class Organizations::Staff::ManageFostersController < Organizations::BaseControl
   def index
     authorize! User, context: {organization: Current.organization},
       with: Organizations::ManageFostersPolicy
+
+    @q = authorized_scope(Match.fosters).ransack(params[:q])
+    @foster_pets = @q.result(distinct: true).includes(:pet).group_by(&:pet)
   end
 end
