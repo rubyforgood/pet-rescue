25afcb72c46a3651df72971ccc6ec11719dbf4a5
mononoken <ken.maeshima@pm.me>
2024-05-17 22:09:15 -0700

    Add start_date and end_date to Fosters factory

--- test/factories/matches.rb
+++ test/factories/matches.rb
@@ -8,6 +8,8 @@ FactoryBot.define do
       pet { association :pet, placement_type: "Fosterable" }
       match_type { :foster }
       adopter_foster_account { association :foster_account, :with_profile }
+      start_date { Faker::Time.between(from: 1.year.ago, to: 1.month.from_now) }
+      end_date { start_date + rand(3..6).months }
     end
   end
 end
