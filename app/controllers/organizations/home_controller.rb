06b0d9ff87394aa35d41e1f4b3258ebabb559396
Meghan Gutshall <meghan.gutshall@gmail.com>
2024-05-31 11:52:18 -0400

    Adjust #index action to only feature pets with photos

--- app/controllers/organizations/home_controller.rb
+++ app/controllers/organizations/home_controller.rb
@@ -2,7 +2,8 @@ class Organizations::HomeController < Organizations::BaseController
   skip_verify_authorized only: %i[index]
 
   def index
-    @pets = Pet.includes(images_attachments: :blob)
+    @pets = Pet.with_photo
+      .includes(images_attachments: :blob)
       .where(organization: Current.organization)
       .sample(4)
   end
