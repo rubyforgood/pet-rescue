83c119d22e5bdd784fd690aae9c0d90a2c8e67bc
jameybash <jameybash@gmail.com>
2024-05-31 11:32:56 -0400

    working tests

--- test/system/adoption_fosterer_test.rb
+++ test/system/adoption_fosterer_test.rb
@@ -3,12 +3,14 @@ require "application_system_test_case"
 class AdoptionFostererTest < ApplicationSystemTestCase
   setup do
     @user = create(:fosterer, :with_profile)
-    @organization = @user.organization
-    @page_text = create(:page_text, :with_image, organization: @organization)
-    Current.organization = @organization
+    @adopter_foster_account = @user.adopter_foster_account
+
+    @org = @user.organization
+    @page_text = create(:page_text, :with_image, organization: @org)
+    Current.organization = @org
 
     @pet = create(:pet)
-    create(:adopter_application, :successful_applicant, pet_id: @pet.id)
+    create(:adopter_application, pet: @pet, adopter_foster_account: @adopter_foster_account, organization: @org)
 
     sign_in @user
   end
@@ -17,7 +19,21 @@ class AdoptionFostererTest < ApplicationSystemTestCase
     should "should show list of user's adoption applications" do
       visit adopter_fosterer_dashboard_index_path
       click_on "Adoption Applications"
-      assert_text "this will fail"
+      assert_text @pet.name
+    end
+
+    should "should let user withdraw and then delete applications from their list" do
+      visit adopter_fosterer_adopter_applications_path
+      assert_text @pet.name
+      assert_text "Under Review"
+      accept_confirm do
+        click_on "Withdraw Application"
+      end
+      assert_text "Withdrawn"
+      accept_confirm do
+        click_on "Delete"
+      end
+      assert_no_text @pet.name
     end
   end
 end
