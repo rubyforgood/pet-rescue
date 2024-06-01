2b46cf8186ab71bee4a305e17c5f23d94449eb60
Jaxon Avena <jaxonavena@gmail.com>
2024-06-01 18:18:01 -0400

    Edit turbo stream and en.yml to include proper flash notices for when both staff and fosterer accounts are deactivated/activated.

--- app/controllers/organizations/staff/fosterers_controller.rb
+++ app/controllers/organizations/staff/fosterers_controller.rb
@@ -11,16 +11,22 @@ class Organizations::Staff::FosterersController < Organizations::BaseController
   def deactivate
     @fosterer_account.deactivate
     respond_to do |format|
-      format.html { redirect_to staff_fosterers_index_path, notice: "Fosterer account deactivated." }
-      format.turbo_stream { render "organizations/staff/fosterers/update" }
+      format.html { redirect_to staff_fosterers_index_path, notice: I18n.t("organizations.adopter_fosterer.fosterers.deactivated") }
+      format.turbo_stream do
+        flash.now[:notice] = I18n.t("organizations.adopter_fosterer.fosterers.deactivated")
+        render "organizations/staff/fosterers/update"
+      end
     end
   end
 
   def activate
     @fosterer_account.activate
     respond_to do |format|
-      format.html { redirect_to staff_fosterers_index_path, notice: "Fosterer account activated." }
-      format.turbo_stream { render "organizations/staff/fosterers/update" }
+      format.html { redirect_to staff_fosterers_index_path, notice: I18n.t("organizations.adopter_fosterer.fosterers.activated") }
+      format.turbo_stream do
+        flash.now[:notice] = I18n.t("organizations.adopter_fosterer.fosterers.activated")
+        render "organizations/staff/fosterers/update"
+      end
     end
   end
 
