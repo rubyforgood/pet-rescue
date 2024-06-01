8781f104de893866eb4b37bf8f68277e0508353b
Jamey Alea <jameybash@gmail.com>
2024-06-01 10:14:15 -0400

    Ability to remove a withdrawn application from list (#785)
    Added a feature to the "adopter applications" page (the version that is part of the fosterer dashboard)
    Now after an application is withdrawn via the "Withdraw Application" button, it is displayed with a "Delete" button which flags the application to no longer show in their list.

--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -21,4 +21,24 @@ class LoginTest < ApplicationSystemTestCase
       assert has_current_path?(staff_dashboard_index_path)
     end
   end
+
+  context "when logging in as a fosterer" do
+    setup do
+      @user = create(:fosterer, :with_profile)
+      @organization = @user.organization
+      @page_text = create(:page_text, :with_image, organization: @organization)
+      Current.organization = @organization
+    end
+
+    should "direct to the user's dashboard" do
+      visit root_url
+      click_on "Log In"
+
+      fill_in "Email", with: @user.email
+      fill_in "Password", with: @user.password
+      click_on "Log in"
+
+      assert has_current_path?(adopter_fosterer_dashboard_index_path)
+    end
+  end
 end
