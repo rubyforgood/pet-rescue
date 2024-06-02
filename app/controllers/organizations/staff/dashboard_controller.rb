3942c9b0e39d1183ec72c08c6233defcca75fccf
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-06-01 23:49:33 -0400

    Removed organization from query as we already have tenant scope

--- app/controllers/organizations/staff/dashboard_controller.rb
+++ app/controllers/organizations/staff/dashboard_controller.rb
@@ -7,20 +7,19 @@ class Organizations::Staff::DashboardController < Organizations::BaseController
     @user = current_user
     @organization = Current.organization
     @hide_footer = true
-    pet_ids = @organization.pets.pluck(:id)
-    @not_completed_not_overdue_tasks_count = Task.where(pet_id: pet_ids).is_not_completed.not_overdue.count
-    @not_completed_overdue_tasks_count = Task.where(pet_id: pet_ids).is_not_completed.overdue.count
+    @not_completed_not_overdue_tasks_count = Task.is_not_completed.not_overdue.count
+    @not_completed_overdue_tasks_count = Task.is_not_completed.overdue.count
   end
 
   def incomplete_tasks
     @organization = Current.organization
     @pagy, @pets = pagy(
-      @organization.pets
-                   .left_joins(:tasks)
-                   .select("pets.*, COUNT(tasks.id) AS incomplete_tasks_count")
-                   .where(tasks: {completed: false})
-                   .where("tasks.due_date IS NULL OR tasks.due_date >= ?", Time.current)
-                   .group("pets.id"),
+      Pet
+      .left_joins(:tasks)
+      .select("pets.*, COUNT(tasks.id) AS incomplete_tasks_count")
+      .where(tasks: {completed: false})
+      .where("tasks.due_date IS NULL OR tasks.due_date >= ?", Time.current)
+      .group("pets.id"),
       items: 5
     )
     @column_name = "Incomplete Tasks"
@@ -35,12 +34,12 @@ class Organizations::Staff::DashboardController < Organizations::BaseController
   def overdue_tasks
     @organization = Current.organization
     @pagy, @pets = pagy(
-      @organization.pets
-                   .left_joins(:tasks)
-                   .select("pets.*, COUNT(tasks.id) AS incomplete_tasks_count")
-                   .where(tasks: {completed: false})
-                   .where("tasks.due_date < ?", Time.current)
-                   .group("pets.id"),
+      Pet
+      .left_joins(:tasks)
+      .select("pets.*, COUNT(tasks.id) AS incomplete_tasks_count")
+      .where(tasks: {completed: false})
+      .where("tasks.due_date < ?", Time.current)
+      .group("pets.id"),
       items: 5
     )
     @column_name = "Overdue Tasks"
