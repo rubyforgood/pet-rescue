ab5e044febb074f5b2e29124e1ee03efc401b110
Jade Stewart <114014697+jadekstewart3@users.noreply.github.com>
2024-06-05 10:38:34 -0600

    * Add Adopters link for adopters index
    * Define adopters in breadcrumbs config
    
    * Create route for adopters index
    
    * Create adopters controller with index action
    
    * Create adopters index view
    
    * Standard rb fix
    
    * Add translations for both adopters and fosterers views
    
    * #753 WIP: Add fosters tab in pet show
    
    * #753 Lessen DB query for matches and fosters
    
    * #753 WIP: Make fosters responsive
    
    Clean up unused files
    
    * #753 Redirect to requesting page after foster delete
    
    * #753 Add translations to pet show fosters tab
    
    * #753 Code housekeeping
    
    * Fosters table alignment
    
    ---------
    
    Co-authored-by: Meghan Gutshall <meghan.gutshall@gmail.com>

--- app/controllers/organizations/staff/manage_fosters_controller.rb
+++ app/controllers/organizations/staff/manage_fosters_controller.rb
@@ -16,6 +16,7 @@ class Organizations::Staff::ManageFostersController < Organizations::BaseControl
     @foster = Match.new(match_params.merge(match_type: :foster))
 
     if @foster.save
+      FosterMailer.reminder(@foster).deliver_later
       redirect_to action: :index
     else
       @pets = Pet.fosterable.order(:name)
@@ -63,7 +64,8 @@ class Organizations::Staff::ManageFostersController < Organizations::BaseControl
     @foster.destroy
 
     flash[:success] = "Foster for #{@foster.pet.name} deleted."
-    redirect_to action: :index, page: params[:page]
+
+    redirect_to request.referer
   end
 
   private
