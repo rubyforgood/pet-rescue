545b0a3cc9f630639b220ba35726861995956623
Jade Stewart <114014697+jadekstewart3@users.noreply.github.com>
2024-06-05 10:38:34 -0600

    Adopters index (#793)
    * Add Adopters link for adopters index
    
    * Define adopters in breadcrumbs config
    
    * Create route for adopters index
    
    * Create adopters controller with index action
    
    * Create adopters index view
    
    * Standard rb fix
    
    * Add translations for both adopters and fosterers views

---
+++ app/controllers/organizations/staff/adopters_controller.rb
@@ -0,0 +1,9 @@
+class Organizations::Staff::AdoptersController < Organizations::BaseController
+  layout "dashboard"
+
+  def index
+    authorize! AdopterFosterAccount, context: {organization: Current.organization}
+
+    @adopter_accounts = authorized_scope(AdopterFosterAccount.adopters)
+  end
+end
