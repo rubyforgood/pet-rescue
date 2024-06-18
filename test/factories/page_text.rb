3a85efe44fa2d7f16e1d84d8831a235c8d8cda27
mononoken <ken.maeshima@pm.me>
2024-06-18 16:15:31 -0700

    Add with_hero_image trait to PageText

--- test/factories/page_text.rb
+++ test/factories/page_text.rb
@@ -4,11 +4,21 @@ FactoryBot.define do
     about { Faker::Lorem.sentence }
     association :organization, factory: :organization
 
+    trait :with_hero_image do
+      after(:create) do |page_text|
+        page_text.hero_image.attach(
+          io: File.open(Rails.root.join("test", "fixtures", "files", "test.png")),
+          filename: "test.png",
+          content_type: "image/png"
+        )
+      end
+    end
+
     trait :with_about_us_image do
       after(:create) do |page_text|
         page_text.about_us_images.attach(
-          io: File.open(Rails.root.join("test", "fixtures", "files", "test.png")),
-          filename: "test.png",
+          io: File.open(Rails.root.join("test", "fixtures", "files", "test2.png")),
+          filename: "test2.png",
           content_type: "image/png"
         )
       end
