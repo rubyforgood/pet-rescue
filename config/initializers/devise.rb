6814ae5d0b9c258218355ff8c5a80ae40db1ee32
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-06-21 15:01:35 -0500

    add: config for devise configuration for setting up custom failures through warden in the devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require "organization_authentication_failure"
 
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
@@ -9,6 +10,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -363,4 +365,8 @@ Devise.setup do |config|
 
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
+
+  config.warden do |manager|
+    manager.failure_app = OrganizationAuthenticationFailure
+  end
 end
