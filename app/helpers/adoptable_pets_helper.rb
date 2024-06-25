84ac7952b3cc3c9fd4d1bc3e8de963da3f349da6
Alex Cohen <alexander.c.3600@gmail.com>
2024-06-25 01:42:21 -0400

    Alexanderc360/issue747 (#797)
    * Added existing filters and paginate to adoptable_pets. Added filter for age and breed
    
    * restored a function I removed by mistake

--- app/helpers/adoptable_pets_helper.rb
+++ app/helpers/adoptable_pets_helper.rb
@@ -1,2 +1,10 @@
 module AdoptablePetsHelper
+  def age_ranges
+    [["Less than 3 months", [3.months.ago.to_date, "none"].join("/")],
+      ["Less than 6 months", [6.months.ago.to_date, "none"].join("/")],
+      ["Less than 1 year", [1.year.ago.to_date, "none"].join("/")],
+      ["Young Adult(1 to 3 years)", [3.years.ago.to_date, 1.year.ago.to_date].join("/")],
+      ["Adult(3 to 7 years)", [7.years.ago.to_date, 3.years.ago.to_date].join("/")],
+      ["Senior(7 years and older)", ["none", 7.years.ago.to_date].join("/")]]
+  end
 end
