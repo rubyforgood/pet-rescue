3d3588f8776fe72aec3c201b074d105326db98f7
mononoken <ken.maeshima@pm.me>
2024-06-25 22:34:01 -0700

    Add yard doc attributes to CardComponent with dry-initializer

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -2,32 +2,40 @@
 
 # Renders a card component
 class CardComponent < ApplicationComponent
+  # @!attribute [r] card_options
+  #   @return [Hash] the card options including CSS classes for the card container.
   option :card_options,
     Types::Hash.schema(
       class?: Types::String
     ),
-    default: proc { {} }
+    default: -> { {} }
 
+  # @!attribute [r] header_options
+  #   @return [Hash] the header options including CSS classes.
   option :header_options,
     Types::Hash.schema(
       class?: Types::String
     ),
-    default: proc { {} }
+    default: -> { {} }
 
+  # @!attribute [r] body_options
+  #   @return [Hash] the body options including CSS classes.
   option :body_options,
     Types::Hash.schema(
       class?: Types::String
     ),
-    default: proc { {} }
+    default: -> { {} }
 
+  # @!attribute [r] image_options
+  #   @return [Hash] the image options including the src for image_tag, CSS classes, URL to wrap around the image, and a default image src.
   option :image_options,
     Types::Hash.schema(
       src?: Types::Nominal::Any,
       class?: Types::String,
       url?: Types::String,
-      default?: Types::String
+      default?: Types::Nominal::Any
     ),
-    default: proc { {} }
+    default: -> { {} }
 
   renders_one :header
   renders_one :body
