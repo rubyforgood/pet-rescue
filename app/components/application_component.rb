2cafcbaf601cc8a7ea5ebd26b183e67d66fd5a60
mononoken <ken.maeshima@pm.me>
2024-06-27 13:45:22 -0700

    Add custom ImageTagSource type

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -6,6 +6,15 @@ class ApplicationComponent < ViewComponent::Base
 
   module Types
     include Dry.Types()
+
+    # Custom types
+    ImageTagSource =
+      Types::String |
+      Types::Symbol |
+      Types::Nil |
+      Types::Instance(ActiveStorage::Attachment) |
+      Types::Instance(ActiveStorage::Attached::One) |
+      Types::Instance(ActiveStorage::Attached::Many)
   end
 
   def filter_attribute(value, allowed_values, default: nil)
