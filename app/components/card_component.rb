1123f55199cc0526c27141765e5bf5638272bd4e
mononoken <ken.maeshima@pm.me>
2024-06-27 13:45:22 -0700

    Add custom ImageTagSource type

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -2,38 +2,30 @@
 
 # Renders a card component
 class CardComponent < ApplicationComponent
-  # @!attribute [r] card_options
-  #   @return [Hash] the card options including CSS classes for the card container.
   option :card_options,
     Types::Hash.schema(
       class?: Types::String.default("card card-hover")
     ),
     default: -> { {} }
 
-  # @!attribute [r] header_options
-  #   @return [Hash] the header options including CSS classes.
   option :header_options,
     Types::Hash.schema(
       class?: Types::String.default("card-header")
     ),
     default: -> { {} }
 
-  # @!attribute [r] body_options
-  #   @return [Hash] the body options including CSS classes.
   option :body_options,
     Types::Hash.schema(
       class?: Types::String.default("card-body")
     ),
     default: -> { {} }
 
-  # @!attribute [r] image_options
-  #   @return [Hash] the image options including the src for image_tag, CSS classes, URL to wrap around the image, and a default image src.
   option :image_options,
     Types::Hash.schema(
-      src?: Types::Nominal::Any,
+      src?: Types::ImageTagSource,
       class?: Types::String.default("card-img-top"),
       url?: Types::String,
-      default?: Types::Nominal::Any.default("coming_soon.jpg")
+      default?: Types::ImageTagSource.default("coming_soon.jpg")
     ),
     default: -> { {} }
 
