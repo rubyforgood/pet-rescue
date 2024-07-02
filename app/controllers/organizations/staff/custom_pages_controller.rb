fb127303bbbdad8dd58a46598fde3f49da41b739
Trevor Robinson <trevor.robi1254@gmail.com>
2024-07-01 21:12:36 -0600

    nested module/classes for custom pages controller

--- app/controllers/organizations/staff/custom_pages_controller.rb
+++ app/controllers/organizations/staff/custom_pages_controller.rb
@@ -1,25 +1,29 @@
-class Organizations::Staff::CustomPagesController < Organizations::BaseController
-  layout "dashboard"
-  before_action :set_custom_page, only: %i[edit update]
-  def edit
-  end
+module Organizations
+  module Staff
+    class CustomPagesController < Organizations::BaseController
+      layout "dashboard"
+      before_action :set_custom_page, only: %i[edit update]
+      def edit
+      end
 
-  def update
-    if @custom_page.update(custom_page_params)
-      redirect_to edit_staff_custom_page_path, notice: t(".success")
-    else
-      redirect_to edit_staff_custom_page_path, alert: @custom_page.errors.full_messages.to_sentence
+      def update
+        if @custom_page.update(custom_page_params)
+          redirect_to edit_staff_custom_page_path, notice: t(".success")
+        else
+          redirect_to edit_staff_custom_page_path, alert: @custom_page.errors.full_messages.to_sentence
+        end
+      end
+
+      private
+
+      def custom_page_params
+        params.require(:custom_page).permit(:hero, :about, :hero_image, :adoptable_pet_info, about_us_images: [])
+      end
+
+      def set_custom_page
+        @custom_page = CustomPage.first
+        authorize! @custom_page
+      end
     end
-  end
-
-  private
-
-  def custom_page_params
-    params.require(:custom_page).permit(:hero, :about, :hero_image, :adoptable_pet_info, about_us_images: [])
-  end
-
-  def set_custom_page
-    @custom_page = CustomPage.first
-    authorize! @custom_page
-  end
+  end    
 end
