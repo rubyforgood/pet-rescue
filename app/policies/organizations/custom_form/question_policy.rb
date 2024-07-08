16f9b7a6f74149608e9f97d5d54ea9ba65df7cb0
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-07-07 22:13:47 -0400

    Modulaize Question policy code and fix link in question form

--- app/policies/organizations/custom_form/question_policy.rb
+++ app/policies/organizations/custom_form/question_policy.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-class Organizations::CustomForm::QuestionPolicy < ApplicationPolicy
-  pre_check :verify_organization!
-  pre_check :verify_active_staff!
+module Organizations
+  module CustomForm
+    class QuestionPolicy < ApplicationPolicy
+      pre_check :verify_organization!
+      pre_check :verify_active_staff!
 
-  alias_rule :new?, :create?, :index?, to: :manage?
+      alias_rule :new?, :create?, :index?, to: :manage?
 
-  def manage?
-    permission?(:manage_questions)
+      def manage?
+        permission?(:manage_questions)
+      end
+    end
   end
 end
