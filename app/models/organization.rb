0ef17cc402b8bb649e2c61513ba6d7d88f896938
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-07-08 22:44:04 -0500

    remove: all page_text occurrences
    this was changed to custom_page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -25,7 +25,6 @@ class Organization < ApplicationRecord
 
   has_one :profile, dependent: :destroy, class_name: "OrganizationProfile", required: true
   has_one :location, through: :profile
-  has_one :page_text, dependent: :destroy
   has_one :form_submission, dependent: :destroy
   has_one :custom_page, dependent: :destroy
 end
