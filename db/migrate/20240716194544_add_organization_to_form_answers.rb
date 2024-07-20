f425e79ab4701049199982e753a3553519a1da51
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-07-19 20:17:48 -0500

    Combine migration files
    Combine migrations UpdateFormAnswersTable and AddOrganizationToFormAnswers into one file. Seeing that they both update the FormAnswers table, there should only be one file.

--- db/migrate/20240716194544_add_organization_to_form_answers.rb
+++
@@ -1,5 +0,0 @@
-class AddOrganizationToFormAnswers < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :form_answers, :organization, null: false, foreign_key: true
-  end
-end
--- db/migrate/20240718195301_update_form_answers_table.rb
+++ db/migrate/20240718195301_update_form_answers_table.rb
@@ -1,5 +1,7 @@
 class UpdateFormAnswersTable < ActiveRecord::Migration[7.1]
   def change
+    add_reference :form_answers, :organization, null: false, foreign_key: true
+
     change_column_null :form_answers, :question_id, true
 
     remove_index :form_answers, :user_id
