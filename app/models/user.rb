4fcaf038b4f878bbb293da7e3b34572413f84141
sparshalc <imailsparsha@gmail.com>
2024-07-25 14:03:06 +0545

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,7 +52,10 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: {scope: :organization_id}
+  validates :email, presence: true, uniqueness: { scope: :organization_id }, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: "should be a valid email address"
+  }
   # validates :tos_agreement, acceptance: {message: "Please accept the Terms and Conditions"},
   #   allow_nil: false, on: :create
 
