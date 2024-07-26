f53bf0709eadd6d3341e5805704f2c6878282bcf
Justin <16829344+jmilljr24@users.noreply.github.com>
2024-07-26 18:56:00 -0400

    869 CSV Import service (#872)
    * Initial outline of import service
    
    * use call
    
    * Add tests
    
    * standard:fix
    
    * change test to use tempfile
    
    * add downcase to user email
    
    * add csv_timestamp column
    
    * add timestamp check for duplicates
    
    * clean up
    
    * dry tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_19_183226) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_25_225443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -168,6 +168,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_19_183226) do
     t.bigint "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "csv_timestamp"
     t.index ["organization_id"], name: "index_form_submissions_on_organization_id"
     t.index ["person_id"], name: "index_form_submissions_on_person_id"
   end
