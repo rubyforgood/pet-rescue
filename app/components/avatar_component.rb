cc009e686cce496af66db8c0d7ef221a67f544d4
Erin Claudio <eclaudio@gmail.com>
2024-08-08 13:25:51 -0400

    Add the user logic back in

--- app/components/avatar_component.rb
+++ app/components/avatar_component.rb
@@ -2,6 +2,7 @@
 
 # Renders a User's avatar as image or user's initials
 class AvatarComponent < ApplicationComponent
+  param :user, Types::Instance(User), optional: true
   option :pet, Types::Instance(Pet), optional: true
   option :like, optional: true
   option :size, Types::Size, default: -> { :md }
@@ -11,13 +12,27 @@ class AvatarComponent < ApplicationComponent
   def avatar
     if pet
       image_tag(url_for(pet.images.first), alt: pet.name, class: image_classes)
+    elsif user
+      image_tag(url_for(image_url), alt: alt, class: image_classes)
     else
-      content_tag(:span, pet_initials, class: initials_classes)
+      content_tag(:span, initials, class: initials_classes)
     end
   end
 
   def pet_initials
     "pet"
+  end
+
+  def image_url
+    @user.avatar.attached? ? url_for(@user.avatar) : nil
+  end
+
+  def initials
+    "#{user.first_name[0]}#{user.last_name[0]}".upcase
+  end
+
+  def alt
+    "#{user.first_name.capitalize}'s avatar"
   end
 
   def container_classes
