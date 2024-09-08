166179b244a6f44f8f7d3f41c0ab96370e316992
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-09-08 16:34:37 -0400

    Add external form to dashboard

--- app/controllers/organizations/adopter_fosterer/external_form_controller.rb
+++ app/controllers/organizations/adopter_fosterer/external_form_controller.rb
@@ -2,9 +2,19 @@ module Organizations
   module AdopterFosterer
     class ExternalFormController < ApplicationController
       skip_verify_authorized # MARK: what should the auth policy look like for this action?
+      layout :form_layout
 
       def index
         @form_url = Current.organization.external_form_url
+      end
+
+      private
+      def form_layout
+        if params[:dashboard]
+          "adopter_foster_dashboard"
+        else
+          "application"
+        end
       end
     end
   end
