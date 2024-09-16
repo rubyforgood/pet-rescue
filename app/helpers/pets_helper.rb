8513980d25c2b82b35179f12779807613a327e75
Italo Matos <italomatos@gmail.com>
2024-09-16 08:47:52 -0300

    code review suggestions

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -4,8 +4,10 @@ module PetsHelper
       render BadgeComponent.new("Adoption Pending", class: "badge bg-info")
     elsif pet.is_adopted?
       render BadgeComponent.new("Adopted", class: "badge bg-warning")
+    elsif pet.in_foster?
+      render BadgeComponent.new("In Foster", class: "badge text-white bg-dark-primary")
     elsif pet.open?
-      render BadgeComponent.new("Open", class: "badge bg-light-warning")
+      render BadgeComponent.new("Open", class: "badge bg-light-warning text-dark")
     else
       ""
     end
