efc431f1b0cfd81ba2e7075b125d563c971fbe51
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-09-26 22:12:33 -0400

    Functionality to show incomplete tasks for adopted pets

---
+++ test/controllers/organizations/adopter_fosterer/adopted_pets/tasks_controller_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+require "action_policy/test_helper"
+
+class Organizations::AdopterFosterer::AdoptedPets::TasksControllerTest < ActionDispatch::IntegrationTest
+  context "authorization" do
+    include ActionPolicy::TestHelper
+
+    setup do
+      @organization = ActsAsTenant.current_tenant
+      @adopter = create(:adopter)
+      sign_in @adopter
+      @pet = create(:pet)
+    end
+
+    context "#index" do
+      should "be authorized" do
+        get adopter_fosterer_adopted_pet_tasks_url(adopted_pet_id: @pet.id)
+        assert_response :success
+      end
+
+      should "assign the requested pet" do
+        get adopter_fosterer_adopted_pet_tasks_url(adopted_pet_id: @pet.id)
+        assert_equal @pet, assigns(:pet)
+      end
+
+      should "assign incomplete tasks" do
+        incomplete_task = create(:task, pet: @pet, completed: false)
+        create(:task, pet: @pet, completed: true)
+
+        get adopter_fosterer_adopted_pet_tasks_url(adopted_pet_id: @pet.id)
+        assert_includes assigns(:tasks), incomplete_task
+        refute_includes assigns(:tasks), Task.where(completed: true)
+      end
+    end
+  end
+end
