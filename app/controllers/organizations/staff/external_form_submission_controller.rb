7e04381c308b08fac37d1c0d4e187ad1f2a46d84
wandergithub <wandergonzalez0@hotmail.com>
2024-10-02 11:47:07 -0400

    change class namespace to nested modules

--- app/controllers/organizations/staff/external_form_submission_controller.rb
+++ app/controllers/organizations/staff/external_form_submission_controller.rb
@@ -1,6 +1,11 @@
-class Organizations::Staff::ExternalFormSubmissionController < Organizations::BaseController
-  layout "dashboard"
-  def index
-    authorize! StaffAccount, context: {organization: Current.organization}
+module Organizations
+  module Staff
+    class ExternalFormSubmissionController < Organizations::BaseController
+      layout "dashboard"
+
+      def index
+        authorize! StaffAccount, context: {organization: Current.organization}
+      end
+    end
   end
 end
