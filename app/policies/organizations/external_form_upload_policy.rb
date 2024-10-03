0d43a0ef984055ecf2d6fd0f779e81c75d4cedab
wandergithub <wandergonzalez0@hotmail.com>
2024-10-03 11:22:59 -0400

    adds pre-checks and removes unnecessary html tag id

--- app/policies/organizations/external_form_upload_policy.rb
+++ app/policies/organizations/external_form_upload_policy.rb
@@ -1,5 +1,8 @@
 module Organizations
   class ExternalFormUploadPolicy < ApplicationPolicy
+    pre_check :verify_organization!
+    pre_check :verify_active_staff!
+    
     def index?
       permission?(:manage_external_form_uploads)
     end
