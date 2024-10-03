c2d07509ca22e189bfe978efbdcad6bb87952809
wandergithub <wandergonzalez0@hotmail.com>
2024-10-03 15:17:04 -0400

    fix linter

--- app/policies/organizations/external_form_upload_policy.rb
+++ app/policies/organizations/external_form_upload_policy.rb
@@ -2,7 +2,7 @@ module Organizations
   class ExternalFormUploadPolicy < ApplicationPolicy
     pre_check :verify_organization!
     pre_check :verify_active_staff!
-    
+
     def index?
       permission?(:manage_external_form_uploads)
     end
