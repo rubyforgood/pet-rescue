b6080ac8e60cefe952585efb9ee09b72cc41f3ed
mononoken <ken.maeshima@pm.me>
2024-10-06 17:01:44 -0700

    Drop staff_accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_06_004058) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_06_233741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -241,16 +241,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_06_004058) do
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
   end
 
-  create_table "staff_accounts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "organization_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "deactivated_at"
-    t.index ["organization_id"], name: "index_staff_accounts_on_organization_id"
-    t.index ["user_id"], name: "index_staff_accounts_on_user_id"
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -328,8 +318,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_06_004058) do
   add_foreign_key "people", "organizations"
   add_foreign_key "pets", "organizations"
   add_foreign_key "questions", "forms"
-  add_foreign_key "staff_accounts", "organizations"
-  add_foreign_key "staff_accounts", "users"
   add_foreign_key "tasks", "pets"
   add_foreign_key "users", "people"
 end
