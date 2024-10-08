06737d65767eb17ea98c854274a73656f1e75973
wandergithub <wandergonzalez0@hotmail.com>
2024-10-07 22:11:53 -0400

    Adds multiple param to partial Modify external form controller to support just one file upload correct controller test

--- app/controllers/organizations/staff/external_form_upload_controller.rb
+++ app/controllers/organizations/staff/external_form_upload_controller.rb
@@ -11,12 +11,9 @@ module Organizations
       def create
         authorize! :external_form_upload,
           context: {organization: Current.organization}
-        files = params.require(:files)
-        files.each do |file|
-          next if !file.is_a?(ActionDispatch::Http::UploadedFile)
-          import_service = Organizations::Importers::GoogleCsvImportService.new(file)
-          import_service.call
-        end
+        file = params.require(:files).first
+        import_service = Organizations::Importers::GoogleCsvImportService.new(file)
+        import_service.call
       end
     end
   end
--- test/controllers/organizations/staff/external_form_upload_controller_test.rb
+++ test/controllers/organizations/staff/external_form_upload_controller_test.rb
@@ -2,23 +2,20 @@ require "test_helper"
 
 class Organizations::Staff::ExternalFormUploadControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @organization = ActsAsTenant.current_tenant
-    @file = fixture_file_upload("google_form_sample.csv", "text/csv")
-    @params = {files: [@file]}
-    @user = create(:admin, email: "adopter1@alta.com")
-    @user2 = create(:admin)
-    sign_in @user
+    file = fixture_file_upload("google_form_sample.csv", "text/csv")
+    @params = {files: [file]}
+    user = create(:admin)
+    @user2 = create(:adopter, email: "adopter1@alta.com")
+    sign_in user
   end
 
-  test "Import from google_form_sample.csv" do
-    # FomAnswers and FormSubmissions should be created
+  test "Import from google_form_sample.csv creates FormAnswers" do
     assert_difference "FormAnswer.count", 4 do
-      assert_difference "FormSubmission.count", 1 do
-        post staff_external_form_upload_index_path, params: @params
-      end
+      post staff_external_form_upload_index_path, params: @params
     end
-    # There should be a form submission for @user but not for @user2
-    # assert FormSubmission.exists?(person_id: @user.id)
-    assert_not FormSubmission.exists?(person_id: @user2.id)
+    # No Form Answer is created when there is already existing Form submition with the csv_timestamp
+    assert_no_difference "FormAnswer.count" do
+      post staff_external_form_upload_index_path, params: @params
+    end
   end
 end
