66463fa3544b36c477e46c874cd3537dfc8db937
wandergithub <wandergonzalez0@hotmail.com>
2024-10-10 14:09:17 -0400

    Add new test and change to nested modules

--- test/controllers/organizations/staff/external_form_upload_controller_test.rb
+++ test/controllers/organizations/staff/external_form_upload_controller_test.rb
@@ -1,21 +1,34 @@
 require "test_helper"
 
-class Organizations::Staff::ExternalFormUploadControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    file = fixture_file_upload("google_form_sample.csv", "text/csv")
-    @params = {files: [file]}
-    user = create(:admin)
-    @user2 = create(:adopter, email: "adopter1@alta.com")
-    sign_in user
-  end
+module Organizations
+  module Staff
+    class ExternalFormUploadControllerTest < ActionDispatch::IntegrationTest
+      setup do
+        file = fixture_file_upload("google_form_sample.csv", "text/csv")
+        @params = {files: [file]}
+        user = create(:admin)
+        @person1 = create(:adopter, email: "adopter1@alta.com").person
+        @person2 = create(:adopter, email: "willnotgetnewanswers@alta.com").person
+        sign_in user
+      end
 
-  test "Import from google_form_sample.csv creates FormAnswers" do
-    assert_difference "FormAnswer.count", 4 do
-      post staff_external_form_upload_index_path, params: @params
-    end
-    # No Form Answer is created when there is already existing Form submition with the csv_timestamp
-    assert_no_difference "FormAnswer.count" do
-      post staff_external_form_upload_index_path, params: @params
+      test "Import from google_form_sample.csv creates FormAnswers" do
+        assert_difference "FormAnswer.count", 4 do
+          post staff_external_form_upload_index_path, params: @params
+        end
+        # No Form Answer is created when there is already existing Form submition with the csv_timestamp
+        assert_no_difference "FormAnswer.count" do
+          post staff_external_form_upload_index_path, params: @params
+        end
+      end
+
+      test "Creates form answers for person1 but not for person2" do
+        assert_changes -> { @person1.form_answers.count } do
+          assert_no_changes -> { @person2.form_answers.count } do
+            post staff_external_form_upload_index_path, params: @params
+          end
+        end
+      end
     end
   end
 end
