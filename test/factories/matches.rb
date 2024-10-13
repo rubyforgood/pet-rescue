389ed2600e56ba3a7a6d33ec9fcdae35e4872f6d
Cory Streiff <90390502+coalest@users.noreply.github.com>
2024-10-13 23:55:00 +0200

    1021 Refactor Pet instance method to use the Match model instead of the AdopterApplication model (#1055)
    * Refactor Pet#is_adopted? method to use Match model
    
    * Improve performance of staff pets index page
    
    * Remove 2 unneeded queries from rendering /staff/pets
    
    * Fix N+1 in AdoptablePetsController
    
    * Fix N+1 for pets in LikesController
    
    * Fix AdoptablePetsController#index action to expose only unadopted pets

--- test/factories/matches.rb
+++ test/factories/matches.rb
@@ -11,5 +11,11 @@ FactoryBot.define do
       start_date { Faker::Time.between(from: 1.year.ago, to: 1.month.from_now) }
       end_date { start_date + rand(3..6).months }
     end
+
+    trait :adoption do
+      match_type { :adoption }
+      start_date { Faker::Time.between(from: 1.year.ago, to: 1.day.ago) }
+      end_date { 1.day.from_now }
+    end
   end
 end
