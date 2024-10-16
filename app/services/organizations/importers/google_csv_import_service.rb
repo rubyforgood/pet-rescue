ad286e487753704f0178b04edb5dd053a9bb65a0
wandergithub <wandergonzalez0@hotmail.com>
2024-10-16 13:35:38 -0400

    - refactors google_csv_import_service - Updates users Factorie to create the person - fix external_form_upload_controller_tests

--- app/services/organizations/importers/google_csv_import_service.rb
+++ app/services/organizations/importers/google_csv_import_service.rb
@@ -15,16 +15,31 @@ module Organizations
           csv_timestamp = Time.parse(row["Timestamp"])
 
           person = Person.find_by(email:, organization: @organization)
-          previous = FormSubmission.where(person:, csv_timestamp:)
-          next unless person && previous.empty?
+          latest_form_submission = person.latest_form_submission
 
-          ActiveRecord::Base.transaction do
-            form_submission = FormSubmission.create!(person:, csv_timestamp:)
-            row.each do |col|
-              next if col[0] == "Email" || col[0] == "Timestamp"
+          if latest_form_submission.form_answers.empty?
+            ActiveRecord::Base.transaction do
+              form_submission = latest_form_submission
+              row.each do |col|
+                next if col[0] == "Email" || col[0] == "Timestamp"
 
-              FormAnswer.create!(form_submission:,
-                question_snapshot: col[0], value: col[1])
+                FormAnswer.create!(form_submission:,
+                  question_snapshot: col[0], value: col[1])
+              end
+            end
+          else
+            existing_submission = FormSubmission.find_by(person:, csv_timestamp:)
+
+            next if existing_submission
+
+            ActiveRecord::Base.transaction do
+              form_submission = FormSubmission.create!(person:, csv_timestamp:)
+              row.each do |col|
+                next if col[0] == "Email" || col[0] == "Timestamp"
+
+                FormAnswer.create!(form_submission:,
+                  question_snapshot: col[0], value: col[1])
+              end
             end
           end
         end
--- test/controllers/organizations/staff/external_form_upload_controller_test.rb
+++ test/controllers/organizations/staff/external_form_upload_controller_test.rb
@@ -7,27 +7,20 @@ module Organizations
         file = fixture_file_upload("google_form_sample.csv", "text/csv")
         @params = {files: [file]}
         user = create(:admin)
-        @person1 = create(:adopter, :with_person, email: "adopter1@alta.com")
-        @person2 = create(:adopter, email: "willnotgetnewanswers@alta.com")
+        @person = create(:adopter, email: "adopter1111@alta.com")
+        @person2 = create(:adopter, email: "no_answer_will_be_created@alta.com")
         sign_in user
       end
 
-      # test "Import from google_form_sample.csv creates FormAnswers" do
-      #   assert_difference "FormAnswer.count", 4 do
-      #     post staff_external_form_upload_index_path, params: @params
-      #   end
-      #   # No Form Answer is created when there is already existing Form submition with the csv_timestamp
-      #   assert_no_difference "FormAnswer.count" do
-      #     post staff_external_form_upload_index_path, params: @params
-      #   end
-      # end
-
-      test "Creates form answers for person1 but not for person2" do
-        assert_changes -> { @person1.latest_form_submission.form_answers.count } do
-          # assert_no_changes -> { @person2.latest_form_submission.form_answers.count } do
+      test "Creates form answers for person in its latest form submission" do
+        assert_changes -> { @person.latest_form_submission.form_answers.count } do
           post staff_external_form_upload_index_path, params: @params
+        end
+      end
 
-          # end
+      test "It does not create form answers for person2" do
+        assert_no_difference -> { @person2.latest_form_submission.form_answers.count } do
+          post staff_external_form_upload_index_path, params: @params
         end
       end
     end
