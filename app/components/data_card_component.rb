ee1ef5392897b89e9cdce730fc7bcb278c98cb60
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-10-18 09:44:51 -0400

    1057 - Staff: Dashboard summary stats visual design improvement (#1060)
    * UI improvement for staff dashboard
    
    * Margin to left of icons to look better
    
    * Updated staff dashboard code to use DataCardComponent
    
    * Customized title size and icon color
    
    * Lint fix
    
    * New line fix

--- app/components/data_card_component.rb
+++ app/components/data_card_component.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class DataCardComponent < ViewComponent::Base
-  def initialize
+  attr_reader :title_size, :icon_color
+
+  def initialize(title_size: :medium, icon_color: nil)
+    @title_size = title_size
+    @icon_color = icon_color
   end
 
   renders_one :title
@@ -13,5 +17,18 @@ class DataCardComponent < ViewComponent::Base
   # Uses feather icons (app/assets/stylesheets/feather.css)
   def feather_classes
     "fe fe-#{icon_name}"
+  end
+
+  def title_size_class
+    case title_size
+    when :small
+      "fs-6" # Equivalent to h4
+    when :medium
+      "fs-4" # Equivalent to h3
+    when :large
+      "fs-3" # Equivalent to h1
+    else
+      "fs-4" # Default to medium
+    end
   end
 end
